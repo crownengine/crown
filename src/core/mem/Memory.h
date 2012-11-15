60d5e92d28cc64950c88a42999a1a4e32d229db7
Daniele Bartolini <danyatk@gmail.com>
2012-11-15 16:52:35 +0100

    Add Allocator interface and MallocAllocator implementation

---
+++ src/core/mem/Memory.h
@@ -0,0 +1,51 @@
+/*
+Copyright (c) 2012 Daniele Bartolini, Simone Boscaratto
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <stdint.h>
+#include <cassert>
+#include <cstdio>
+#include "Types.h"
+
+namespace crown
+{
+namespace memory
+{
+
+const uint		PADDING_VALUE	= 0xFFFFFFFFu;	//!< Value used to fill unused memory
+const size_t	DEFAULT_ALIGN	= 4;			//!< Default memory alignment in bytes
+
+//! Returns the pointer p aligned to the desired align
+inline void* align(void* p, size_t align)
+{
+	uintptr_t ptr = (uintptr_t)p;
+
+	return (void*)(ptr + (align - ptr % align));
+}
+
+} // namespace memory
+} // namespace crown
+
