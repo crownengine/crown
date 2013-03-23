74f95369baa0752bc16e6a6403bc26b0d2dbbb2c
mikymod <mikymod@gmail.com>
2013-03-23 18:16:46 +0100

    CrownEnum added in android

---
+++ android/src/crown/android/CrownEnum.java
@@ -0,0 +1,16 @@
+package crown.android;
+
+public class CrownEnum
+{
+	// OSEventType enum in OS.h
+	public static final int OSET_NONE			= 0;
+	public static final int OSET_KEY_PRESS		= 1;
+	public static final int OSET_KEY_RELEASE	= 2;
+	public static final int OSET_BUTTON_PRESS	= 3;
+	public static final int OSET_BUTTON_RELEASE	= 4;
+	public static final int OSET_MOTION_NOTIFY	= 5;
+	public static final int OSET_TOUCH_DOWN 	= 6;
+	public static final int OSET_TOUCH_MOVE		= 7;
+	public static final int OSET_TOUCH_UP		= 8;
+	public static final int OSET_ACCELEROMETER	= 9; 
+}
