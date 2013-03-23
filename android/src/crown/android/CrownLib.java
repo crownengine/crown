025a29af0f235b9488b07e316baa654fec11b2e3
Daniele Bartolini <danyatk@gmail.com>
2013-03-23 07:02:50 +0100

    Declare initAssetManager

--- android/src/crown/android/CrownLib.java
+++ android/src/crown/android/CrownLib.java
@@ -1,11 +1,15 @@
 package crown.android;
 
+import android.content.res.AssetManager;
+
 public class CrownLib
 {
 	static 
 	{
 		System.loadLibrary("crown");
 	}
+
+	public static native void initAssetManager(AssetManager assetManager);
 
 	public static native void pushEvent(int type, int a, int b, int c, int d);
 }
