775bfccc3bb9ffaaaad7787bf2fae804e209463c
Daniele Bartolini <danyatk@gmail.com>
2013-04-13 22:39:03 +0200

    Rename Material to MaterialResource

--- src/MaterialResource.cpp
+++ src/MaterialResource.cpp
@@ -23,8 +23,8 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include "Material.h"
 #include "Types.h"
+#include "MaterialResource.h"
 
 namespace crown
 {
--- src/MaterialResource.h
+++ src/MaterialResource.h
@@ -30,6 +30,8 @@ OTHER DEALINGS IN THE SOFTWARE.
 #include "Resource.h"
 #include "Vec3.h"
 #include "Color4.h"
+#include "Material.h"
+#include "Texture.h"
 
 namespace crown
 {
@@ -38,79 +40,6 @@ namespace crown
 //! Note that the maximum number of usable layers
 //! depends on the graphic card.
 const uint32_t MAX_TEXTURE_LAYERS = 8;
-
-enum FogMode
-{
-	FM_LINEAR	= 0,
-	FM_EXP		= 1,
-	FM_EXP2		= 2,
-	FM_COUNT
-};
-
-enum CompareFunction
-{
-	CF_NEVER	= 0,
-	CF_LESS		= 1,
-	CF_EQUAL	= 2,
-	CF_LEQUAL	= 3,
-	CF_GREATER	= 4,
-	CF_NOTEQUAL	= 5,
-	CF_GEQUAL	= 6,
-	CF_ALWAYS	= 7,
-	CF_COUNT
-};
-
-enum ShadingType
-{
-	ST_FLAT		= 0,
-	ST_SMOOTH	= 1,
-	ST_COUNT
-};
-
-enum PolygonMode
-{
-	PM_POINT	= 0,
-	PM_LINE		= 1,
-	PM_FILL		= 2,
-	PM_COUNT
-};
-
-enum FrontFace
-{
-	FF_CW		= 0,
-	FF_CCW		= 1,
-	FF_COUNT
-};
-
-enum BlendEquation
-{
-	BE_FUNC_ADD					= 0,
-	BE_FUNC_SUBTRACT			= 1,
-	BE_FUNC_REVERSE_SUBTRACT	= 2,
-	BE_MIN						= 3,
-	BE_MAX						= 4,
-	BE_COUNT
-};
-
-enum BlendFunction
-{
-	BF_ZERO						= 0,
-	BF_ONE						= 1,
-	BF_SRC_COLOR				= 2,
-	BF_ONE_MINUS_SRC_COLOR		= 3,
-	BF_DST_COLOR				= 4,
-	BF_ONE_MINUS_DST_COLOR		= 5,
-	BF_SRC_ALPHA				= 6,
-	BF_ONE_MINUS_SRC_ALPHA		= 7,
-	BF_DST_ALPHA				= 8,
-	BF_ONE_MINUS_DST_ALPHA		= 9,
-	BF_CONSTANT_COLOR			= 10,
-	BF_ONE_MINUS_CONSTANT_COLOR	= 11,
-	BF_CONSTANT_ALPHA			= 12,
-	BF_ONE_MINUS_CONSTANT_ALPHA	= 13,
-	BF_SRC_ALPHA_SATURATE		= 14,
-	BF_COUNT
-};
 
 class MaterialResource
 {
