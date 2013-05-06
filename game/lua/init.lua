a0a0e3790db244a25a15cff48aed937f981092c8
mikymod <mikymod@gmail.com>
2013-05-06 17:05:16 +0200

    init.lua updated

--- game/lua/init.lua
+++ game/lua/init.lua
@@ -1,5 +1,7 @@
 require("vec3")
+require("mat4")
 require("quat")
+
 
 local ffi = require("ffi")
 
@@ -9,6 +11,6 @@ lib = ffi.load(lib_path .. "libcrown.so", true)
 
 local v = lib.vec3(1.0, 1.0, 1.0)
 local q = lib.quat(10.0, v)
+local m = lib.mat4(1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0)
 
-print(q.w)
-print(q.v.x)
+lib.mat4_print(m)
