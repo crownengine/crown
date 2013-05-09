bca47067928d96467c409d67ec273fd407038abc
Daniele Bartolini <danyatk@gmail.com>
2013-05-09 14:14:43 +0200

    Add keycodes for digits and upper/lower-case ascii alphabet

--- src/input/KeyCode.h
+++ src/input/KeyCode.h
@@ -30,6 +30,8 @@ namespace crown
 
 const uint16_t MAX_KEYCODES = 256;
 
+typedef uint8_t Key;
+
 /// A KeyCode is the number which uniquely identifies a key
 /// on the keyboard.
 enum KeyCode
@@ -89,13 +91,76 @@ enum KeyCode
 	KC_RSUPER		= 0xB8,
 
 	/* [0x30, 0x39] reserved for ASCII digits */
-	/* [0x41, 0x5A] and [0x61, 0x7A] reserved for ASCII alphabet */
+	KC_0			= 0x30,
+	KC_1			= 0x31,
+	KC_2			= 0x32,
+	KC_3			= 0x33,
+	KC_4			= 0x34,
+	KC_5			= 0x35,
+	KC_6			= 0x36,
+	KC_7			= 0x37,
+	KC_8			= 0x38,
+	KC_9			= 0x39,
+
+	/* [0x41, 0x5A] reserved for ASCII alphabet */
+	KC_A			= 0x41,
+	KC_B			= 0x42,
+	KC_C			= 0x43,
+	KC_D			= 0x44,
+	KC_E			= 0x45,
+	KC_F			= 0x46,
+	KC_G			= 0x47,
+	KC_H			= 0x48,
+	KC_I			= 0x49,
+	KC_J			= 0x4A,
+	KC_K			= 0x4B,
+	KC_L			= 0x4C,
+	KC_M			= 0x4D,
+	KC_N			= 0x4E,
+	KC_O			= 0x4F,
+	KC_P			= 0x50,
+	KC_Q			= 0x51,
+	KC_R			= 0x52,
+	KC_S			= 0x53,
+	KC_T			= 0x54,
+	KC_U			= 0x55,
+	KC_V			= 0x56,
+	KC_W			= 0x57,
+	KC_X			= 0x58,
+	KC_Y			= 0x59,
+	KC_Z			= 0x5A,
+
+	/* [0x61, 0x7A] reserved for ASCII alphabet */
+	KC_a			= 0x61,
+	KC_b			= 0x62,
+	KC_c			= 0x63,
+	KC_d			= 0x64,
+	KC_e			= 0x65,
+	KC_f			= 0x66,
+	KC_g			= 0x67,
+	KC_h			= 0x68,
+	KC_i			= 0x69,
+	KC_j			= 0x6A,
+	KC_k			= 0x6B,
+	KC_l			= 0x6C,
+	KC_m			= 0x6D,
+	KC_n			= 0x6E,
+	KC_o			= 0x6F,
+	KC_p			= 0x70,
+	KC_q			= 0x71,
+	KC_r			= 0x72,
+	KC_s			= 0x73,
+	KC_t			= 0x74,
+	KC_u			= 0x75,
+	KC_v			= 0x76,
+	KC_w			= 0x77,
+	KC_x			= 0x78,
+	KC_y			= 0x79,
+	KC_z			= 0x7A,
 
 	// The last key _must_ be <= 0xFF
 	KC_COUNT		= 0xFF
 };
-
-typedef uint8_t Key;
 
 } // namespace crown
 
