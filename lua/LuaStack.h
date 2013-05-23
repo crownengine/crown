ab7dda3a7a6bc391a5402104421fe095d6286662
mikymod <mikymod@gmail.com>
2013-05-23 20:17:49 +0200

    LuaStack moved to lua dir

---
+++ lua/LuaStack.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "lua.hpp"
+#include "Types.h"
+
+namespace crown
+{
+
+class LuaStack
+{
+public:
+
+							LuaStack(lua_State* L);
+
+	void					push_bool(bool value);
+
+	void					push_int(int32_t value);
+
+	void 					push_float(float value);
+
+	void 					push_string(const char* str, size_t len);
+
+	void					push_lightudata(void* ptr);
+
+	bool 					get_bool(int32_t index);
+
+	int32_t					get_int(int32_t index);
+
+	float 					get_float(int32_t index);
+
+	const char*				get_string(int32_t index);
+
+	const void*				get_lightudata(int32_t index);
+
+private:
+
+	lua_State* 				m_state;
+};
+
+} // namespace crown
