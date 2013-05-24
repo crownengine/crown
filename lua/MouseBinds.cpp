46c29c185ea440c3e8783f780837fad76efd69b9
Daniele Bartolini <danyatk@gmail.com>
2013-05-24 09:19:53 +0200

    Add mouse lua bindings - need additional work

--- lua/MouseBinds.cpp
+++ lua/MouseBinds.cpp
@@ -1,108 +1,85 @@
+#include "LuaStack.h"
 #include "Device.h"
-#include "ScriptSystem.h"
+#include "LuaEnvironment.h"
 #include "Mouse.h"
-#include "OS.h"
+#include "Log.h"
 
 namespace crown
 {
 
 extern "C"
 {
-	bool	mouse_button_pressed(uint32_t button);
 
-	bool	mouse_button_released(uint32_t button);
+extern Vec2* next_vec2();
 
-	Vec2&	mouse_cursor_xy();
-
-	void	mouse_set_cursor_xy(const Vec2& position);
-
-	Vec2&	mouse_cursor_relative_xy();
-
-	void	mouse_set_cursor_relative_xy(const Vec2& position);
-}
-
-bool mouse_button_pressed(uint32_t button)
-{	
-	MouseButton b;
-
-	switch(button)
-	{
-		case MB_LEFT:		
-		{
-			b = MB_LEFT;
-		}
-		case MB_MIDDLE:
-		{
-			b = MB_MIDDLE;
-		}
-		case MB_RIGHT:
-		{
-			b = MB_RIGHT;
-		}
-		default:
-		{
-			return false;
-		}
-	}
-
-	return device()->mouse()->button_pressed(b);
-}
-
-bool mouse_button_released(uint32_t button)
+//-----------------------------------------------------------------------------
+int32_t mouse_button_pressed(lua_State* L)
 {
-	MouseButton b;
+	LuaStack stack(L);
 
-	switch(button)
-	{
-		case MB_LEFT:		
-		{
-			b = MB_LEFT;
-		}
-		case MB_MIDDLE:
-		{
-			b = MB_MIDDLE;
-		}
-		case MB_RIGHT:
-		{
-			b = MB_RIGHT;
-		}
-		default:
-		{
-			return false;
-		}
-	}
+	int32_t button = stack.get_int(1);
 
+	stack.push_bool(device()->mouse()->button_pressed((MouseButton) button));
 
-	return device()->mouse()->button_released(b);
+	return 1;
 }
 
-Vec2& mouse_cursor_xy()
+//-----------------------------------------------------------------------------
+int32_t mouse_button_released(lua_State* L)
 {
+	LuaStack stack(L);
 
-	Point2 tmp = device()->mouse()->cursor_xy();
+	int32_t button = stack.get_int(1);
 
-	return device()->script_system()->next_vec2(tmp.x, tmp.y);
+	stack.push_bool(device()->mouse()->button_released((MouseButton) button));
+
+	return 1;
 }
 
-void mouse_set_cursor_xy(const Vec2& position)
+//-----------------------------------------------------------------------------
+int32_t mouse_cursor_xy(lua_State* L)
 {
-	Point2 tmp;
-	tmp.x = (uint32_t)position.x;
-	tmp.y = (uint32_t)position.y;
-
-	device()->mouse()->set_cursor_xy(tmp);
 }
 
-Vec2& mouse_cursor_relative_xy()
+//-----------------------------------------------------------------------------
+int32_t mouse_set_cursor_xy(lua_State* L)
 {
-	Vec2 tmp = device()->mouse()->cursor_relative_xy();
-
-	return device()->script_system()->next_vec2(tmp.x, tmp.y);
 }
 
-void mouse_set_cursor_relative_xy(const Vec2& position)
+//-----------------------------------------------------------------------------
+int32_t mouse_cursor_relative_xy(lua_State* L)
 {
-	device()->mouse()->set_cursor_relative_xy(position);
+	LuaStack stack(L);
+
+	Vec2* xy = next_vec2();
+	*xy = device()->mouse()->cursor_relative_xy();
+
+	stack.push_lightudata(xy);
+
+	return 1;
 }
 
+//-----------------------------------------------------------------------------
+int32_t mouse_set_cursor_relative_xy(lua_State* L)
+{
+	LuaStack stack(L);
+
+	Vec2* xy = (Vec2*) stack.get_lightudata(1);
+
+	device()->mouse()->set_cursor_relative_xy(*xy);
+
+	return 0;
 }
+
+} // extern "C"
+
+//-----------------------------------------------------------------------------
+void load_mouse(LuaEnvironment& env)
+{
+	env.load_module_function("Mouse", "button_pressed",			mouse_button_pressed);
+	env.load_module_function("Mouse", "button_released",		mouse_button_released);
+	env.load_module_function("Mouse", "cursor_relative_xy",		mouse_cursor_relative_xy);
+	env.load_module_function("Mouse", "set_cursor_relative_xy",	mouse_set_cursor_relative_xy);
+}
+
+} // namespace crown
