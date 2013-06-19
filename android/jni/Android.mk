e8f08779e6daee7dc108e18abb35bf76858a57c4
mikymod <mikymod@gmail.com>
2013-06-19 18:51:10 +0200

    Android.mk updated

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -21,7 +21,16 @@ LOCAL_SRC_FILES :=\
 	core/bv/Circle.cpp\
 	core/bv/Frustum.cpp\
 	core/bv/Rect.cpp\
+	core/compressors/ZipCompressor.cpp\
 	core/containers/Generic.cpp\
+	core/filesystem/BinaryReader.cpp\
+	core/filesystem/BinaryWriter.cpp\
+	core/filesystem/DiskFile.cpp\
+	core/filesystem/File.cpp\
+	core/filesystem/Filesystem.cpp\
+	core/filesystem/MemoryFile.cpp\
+	core/filesystem/TextReader.cpp\
+	core/filesystem/TextWriter.cpp\
 	core/math/Color4.cpp\
 	core/math/Mat3.cpp\
 	core/math/Mat4.cpp\
@@ -32,13 +41,26 @@ LOCAL_SRC_FILES :=\
 	core/math/Vec2.cpp\
 	core/math/Vec3.cpp\
 	core/math/Vec4.cpp\
-	core/mem/MallocAllocator.cpp\
-	core/streams/FileStream.cpp\
-	core/streams/MemoryStream.cpp\
-	core/streams/Stream.cpp\
+	core/mem/Allocator.cpp\
+	core/mem/HeapAllocator.cpp\
+	core/mem/LinearAllocator.cpp\
+	core/mem/ProxyAllocator.cpp\
+	core/mem/StackAllocator.cpp\
+	core/mem/Memory.cpp\
+	core/settings/FloatSetting.cpp\
+	core/settings/IntSetting.cpp\
+	core/settings/StringSetting.cpp\
+	core/Args.cpp\
+	core/Log.cpp\
 \
+	input/Accelerometer.cpp\
+	input/Keyboard.cpp\
+	input/Mouse.cpp\
+	input/Touch.cpp\
 	input/EventDispatcher.cpp\
 	input/InputManager.cpp\
+\
+	network/BitMessage.cpp\
 \
 	os/OS.cpp\
 	os/android/AndroidOS.cpp\
@@ -51,58 +73,50 @@ LOCAL_SRC_FILES :=\
 	os/posix/TCPSocket.cpp\
 	os/posix/UDPSocket.cpp\
 \
-	Filesystem.cpp\
-\
-	renderers/gles/GLESIndexBuffer.cpp\
 	renderers/gles/GLESRenderer.cpp\
 	renderers/gles/GLESUtils.cpp\
-	renderers/gles/GLESVertexBuffer.cpp\
+	renderers/DebugRenderer.cpp\
+	renderers/PixelFormat.cpp\
+	renderers/VertexFormat.cpp\
 \
-	Camera.cpp\
-	Device.cpp\
-	Font.cpp\
-	Image.cpp\
-	Log.cpp\
-	MaterialResource.cpp\
-	MovableCamera.cpp\
-	Pixel.cpp\
-	ResourceManager.cpp\
-	ResourceLoader.cpp\
-	FileResourceArchive.cpp\
-	ArchiveResourceArchive.cpp\
-	TextureResource.cpp\
-	TextResource.cpp\
-	Skybox.cpp\
-	FPSSystem.cpp\
-\
-	samples/android/triangle.cpp\
+	ArchiveBundle.cpp
+	Camera.cpp
+	Device.cpp
+	FileBundle.cpp
+	FontResource.cpp
+	FPSSystem.cpp
+	Game.cpp
+	JSONParser.cpp
+	MaterialResource.cpp
+	PixelShaderResource.cpp
+	ResourceManager.cpp
+	TextResource.cpp
+	TextureResource.cpp
+	VertexShaderResource.cpp
 \
 
 LOCAL_C_INCLUDES	:=\
+	$(LOCAL_PATH)/\
 	$(LOCAL_PATH)/core\
-	$(LOCAL_PATH)/core/math\
+	$(LOCAL_PATH)/core/bv\
 	$(LOCAL_PATH)/core/compressors\
 	$(LOCAL_PATH)/core/containers\
-	$(LOCAL_PATH)/core/strings\
-	$(LOCAL_PATH)/core/threads\
-	$(LOCAL_PATH)/core/bv\
+	$(LOCAL_PATH)/core/filesystem\
+	$(LOCAL_PATH)/core/math\
 	$(LOCAL_PATH)/core/mem\
-	$(LOCAL_PATH)/core/streams\
-	$(LOCAL_PATH)/core/threads\
+	$(LOCAL_PATH)/core/filesystem\
 	$(LOCAL_PATH)/core/settings\
-	$(LOCAL_PATH)/loaders\
+	$(LOCAL_PATH)/core/strings\
+	$(LOCAL_PATH)/input\
+	$(LOCAL_PATH)/network\
 	$(LOCAL_PATH)/os\
 	$(LOCAL_PATH)/os/android\
 	$(LOCAL_PATH)/renderers\
-	$(LOCAL_PATH)/things\
-	$(LOCAL_PATH)/filesystem\
 	$(LOCAL_PATH)/renderers/gles\
 	$(LOCAL_PATH)/renderers/gles/egl\
-	$(LOCAL_PATH)/input\
 	$(LOCAL_PATH)/samples\
 
-
 LOCAL_CPPFLAGS	:= -g -fexceptions
-LOCAL_LDLIBS	:= -llog -landroid -lGLESv1_CM
+LOCAL_LDLIBS	:= -llog -landroid -lGLESv2
 include $(BUILD_SHARED_LIBRARY)
 #(call import-module, android/native_app_glue)
