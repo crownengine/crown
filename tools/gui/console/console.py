f0cebda1f595adbae90265edb8b4ab0a1e2bcf7c
mikymod <mikymod@gmail.com>
2013-06-28 11:23:40 +0200

    add network function to console

--- tools/gui/console/console.py
+++ tools/gui/console/console.py
@@ -3,18 +3,11 @@ import os
 import socket
 
 from gi.repository import Gtk
-from gi.repository import Gdk
-
-from pycrown import Repository
-    
-
-
 
 #------------------------------------------------------------------------------
 class Console:
 
-    def __init__(self):
-        
+    def __init__(self):   
         builder = Gtk.Builder()
         builder.add_from_file("ui/console.glade")
         
@@ -28,27 +21,27 @@ class Console:
 
         builder.connect_signals(self)
 
-        #m_sock = socket.create_connection(('localhost', 10000))
+        #self.m_sock = socket.create_connection(('localhost', 10000))
 
         Gtk.main()
     
     def on_destroy(self, *args):
-
-        #m_sock.close()
+        #self.m_sock.close()
         Gtk.main_quit(*args)
 
     def on_key_pressed(self, entry, event):
-        if event.keyval == 0xff0d:
+        # If return is pressed, run command
+        if event.keyval == 0xff0d:  
             self.run_command(entry.get_text())
 
     def run_command(self, cmd):
+        # Send command to Crown
+            #self.m_sock.send(cmd)
+        # Print command to console
         end_iter = self.m_buffer.get_end_iter()
         a_string = "> " + cmd + "\n"
         self.m_buffer.insert(end_iter, a_string, len(a_string))
-
         self.m_entry.set_text("")
-
-
     
 
 #------------------------------------------------------------------------------
