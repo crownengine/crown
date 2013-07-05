ff44562f2eb0073df49b3467ced57c33259c1db3
Daniele Bartolini <danyatk@gmail.com>
2013-07-06 00:14:00 +0200

    Fix CE_ASSERT in release mode

--- src/core/Assert.h
+++ src/core/Assert.h
@@ -37,7 +37,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 	#define CE_ASSERT(condition, message, ...) do { if (!(condition)) { CE_ERROR(__FILE__, __LINE__,\
 				"Assertion failed: %s\n\t" message, #condition, ##__VA_ARGS__); } } while(0)
 #else
-	#define CE_ASSERT(condition, message, ...) ((void)0)
+	#define CE_ASSERT(...) ((void)0)
 #endif
 
 #define CE_ASSERT_NOT_NULL(x) CE_ASSERT(x != NULL, "Parameter must be not null")
