46c777648e1a58d721ce175ee2eb0f4074afece4
Daniele Bartolini <danyatk@gmail.com>
2013-07-16 23:33:13 +0200

    Stop Device when ESC key is pressed

--- samples/01.hello-world/lua/game.lua
+++ samples/01.hello-world/lua/game.lua
@@ -5,7 +5,7 @@ end
 
 function frame(dt)
 	-- Stop the engine when any key is released
-	if Keyboard.any_released() then
+	if Keyboard.key_released(Keyboard.KC_ESCAPE) then
 		Device.stop()
 	end
 end
