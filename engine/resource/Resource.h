714e4396eca747038514666198ac9029f8893fb0
mikymod <mikymod@gmail.com>
2013-07-31 10:56:49 +0200

    implement sound resource

--- engine/resource/Resource.h
+++ engine/resource/Resource.h
@@ -41,6 +41,7 @@ const char* const TEXT_EXTENSION			= "txt";
 const char* const MATERIAL_EXTENSION		= "material";
 const char* const VERTEX_SHADER_EXTENSION	= "vs";
 const char* const PIXEL_SHADER_EXTENSION	= "ps";
+const char* const SOUND_EXTENSION			= "wav";
 
 const uint32_t TEXTURE_TYPE					= 0x1410A16A;
 const uint32_t MESH_TYPE					= 0xE8239EEC;
@@ -49,6 +50,8 @@ const uint32_t TEXT_TYPE					= 0x9000BF0B;
 const uint32_t MATERIAL_TYPE				= 0x46807A92;
 const uint32_t VERTEX_SHADER_TYPE			= 0xDC7F061F;
 const uint32_t PIXEL_SHADER_TYPE			= 0x2A461B45;
+const uint32_t SOUND_TYPE					= 0x8E128AA1;
+
 
 /// Enumerates the loading states of a resource
 enum ResourceState
