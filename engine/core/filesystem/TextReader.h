a29bcd3479c0af8391ce7ee85fa2f94e33c904bd
Daniele Bartolini <danyatk@gmail.com>
2013-08-27 13:42:44 +0200

    Fix TextReader

--- engine/core/filesystem/TextReader.h
+++ engine/core/filesystem/TextReader.h
@@ -37,7 +37,7 @@ class TextReader
 public:
 
 	//-----------------------------------------------------------------------------
-	TextReader::TextReader(File& file) : m_file(file)
+	TextReader(File& file) : m_file(file)
 	{
 	}
 
@@ -49,7 +49,7 @@ public:
 	/// a valid character and therefore it is included in the string copied to string.
 	/// A null character is automatically appended in str after the characters read to
 	/// signal the end of the C string.
-	size_t TextReader::read_string(char* string, size_t size)
+	size_t read_string(char* string, size_t size)
 	{
 		char current_char;
 		size_t bytes_read = 0;
