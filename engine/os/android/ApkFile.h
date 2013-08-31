4fd6388b42daed357dbc41f08f8c85a7e5a099c6
Daniele Bartolini <danyatk@gmail.com>
2013-08-31 16:25:39 +0200

    Fix ApkFile documentation

--- engine/os/android/ApkFile.h
+++ engine/os/android/ApkFile.h
@@ -35,22 +35,50 @@ class ApkFile : public File
 {
 public:
 
+	/// Opens the given @a filename.
 				ApkFile(const char* filename);
 				~ApkFile();
 
+	/// @copydoc File::seek()
 	void		seek(size_t position);
+
+	/// @copydoc File::seek_to_end()
 	void		seek_to_end();
+
+	/// @copydoc File::skip()
 	void		skip(size_t bytes);
+
+	/// @copydoc File::read()
 	void		read(void* buffer, size_t size);
+
+	/// @copydoc File::write()
 	void		write(const void* buffer, size_t size);
+
+	/// @copydoc File::copy_to()
 	bool		copy_to(File& file, size_t size = 0);
+
+	/// @copydoc File::flush()
 	void		flush();
+
+	/// @copydoc File::is_valid()
 	bool		is_valid() const;
+
+	/// @copydoc File::end_of_file()
 	bool		end_of_file() const;
+
+	/// @copydoc File::size()
 	size_t		size() const;
+
+	/// @copydoc File::position()
 	size_t		position() const;
+
+	/// @copydoc File::can_read()
 	bool		can_read() const;
+
+	/// @copydoc File::can_write()
 	bool		can_write() const;
+
+	/// @copydoc File::can_seek()
 	bool		can_seek() const;
 
 private:
