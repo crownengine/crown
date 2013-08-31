871eb4a7012682cb9f334f1774efaa7785b01d9e
Daniele Bartolini <danyatk@gmail.com>
2013-08-31 09:50:22 +0200

    Improve assert message in DiskFile

--- engine/core/filesystem/DiskFile.cpp
+++ engine/core/filesystem/DiskFile.cpp
@@ -83,7 +83,7 @@ void DiskFile::read(void* buffer, size_t size)
 	}
 
 	size_t bytes_read = m_file.read(buffer, size);
-	CE_ASSERT(bytes_read == size, "Failed to read from file");
+	CE_ASSERT(bytes_read == size, "Failed to read from file: requested: %lu, read: %lu", size, bytes_read);
 }
 
 //-----------------------------------------------------------------------------
@@ -98,7 +98,7 @@ void DiskFile::write(const void* buffer, size_t size)
 	}
 
 	size_t bytes_written = m_file.write(buffer, size);
-	CE_ASSERT(bytes_written == size, "Failed to write to file");
+	CE_ASSERT(bytes_written == size, "Failed to write to file: requested: %lu, written: %lu", size, bytes_written);
 }
 
 //-----------------------------------------------------------------------------
