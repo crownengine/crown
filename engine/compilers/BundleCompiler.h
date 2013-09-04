202457ad170c5b09273a5143c9229173bfeb9125
Daniele Bartolini <danyatk@gmail.com>
2013-09-04 19:35:44 +0200

    Add MeshCompiler to BundleCompiler

--- engine/compilers/BundleCompiler.h
+++ engine/compilers/BundleCompiler.h
@@ -26,6 +26,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 
 #pragma once
 
+#include "MeshCompiler.h"
 #include "TextureCompiler.h"
 #include "LuaCompiler.h"
 #include "PackageCompiler.h"
@@ -50,6 +51,7 @@ private:
 
 private:
 
+	MeshCompiler	m_mesh;
 	TextureCompiler	m_texture;
 	LuaCompiler 	m_lua;
 	PackageCompiler m_package;
