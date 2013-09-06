5bf9db67364ccf0819bd30b162c785fc1c55b3d5
Daniele Bartolini <danyatk@gmail.com>
2013-09-06 16:09:46 +0200

    Remove set_android_window(), use shared global variable instead

--- engine/os/android/OsWindow.cpp
+++ engine/os/android/OsWindow.cpp
@@ -32,20 +32,14 @@ OTHER DEALINGS IN THE SOFTWARE.
 namespace crown
 {
 
-static ANativeWindow* g_android_window = NULL;
+ANativeWindow* g_android_window = NULL;
 
 //-----------------------------------------------------------------------------
-OsWindow::OsWindow(uint32_t /*width*/, uint32_t /*height*/, uint32_t /*parent*/) :
-	m_x(0),
-	m_y(0),
-	m_width(0),
-	m_height(0)
+OsWindow::OsWindow(uint32_t /*width*/, uint32_t /*height*/, uint32_t /*parent*/)
+	: m_x(0), m_y(0), m_width(0), m_height(0)
 {
-
 	m_width = ANativeWindow_getWidth(g_android_window);
 	m_height = ANativeWindow_getHeight(g_android_window);
-
-    set_android_window(g_android_window);
 }
 
 //-----------------------------------------------------------------------------
