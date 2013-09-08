f40473fa6e3daac85b2f69fc11cc13daf8afc7f6
mikymod <mikymod@gmail.com>
2013-09-09 00:34:29 +0200

    update Java classes

---
+++ engine/os/android/java/CrownActivity.java
@@ -0,0 +1,149 @@
+/*
+Copyright (c) 2013 Daniele Bartolini, Michele Rossi
+Copyright (c) 2012 Daniele Bartolini, Simone Boscaratto
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package crown.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.view.MotionEvent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.content.Context;
+import android.widget.Toast;
+import android.content.res.AssetManager;
+import android.view.View;
+import android.view.Surface;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+import crown.android.CrownEnum;
+
+/**
+*	BootStrap of Android Application
+*/
+public class CrownActivity extends Activity
+{
+
+	// Debug
+	public static String TAG = "crown";
+
+	// Resource attributes
+    static AssetManager 		mAssetManager;
+
+	// Input attributes
+	private CrownTouch 			mTouch;
+	private CrownSensor			mSensor;
+
+	// Graphic attributes
+	static CrownSurfaceView		mWindow;
+
+//-----------------------------------------------------------------------------
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+		// init AssetManager
+		mAssetManager = getAssets();
+		CrownLib.initAssetManager(mAssetManager);
+
+		// init Native Window
+        mWindow = new CrownSurfaceView(this);
+        setContentView(mWindow);
+
+		// Init Input
+		mTouch = new CrownTouch(this);
+		mSensor = new CrownSensor(this);
+
+		Log.i(TAG, "Crown Activity created");
+    }
+
+//-----------------------------------------------------------------------------
+	public void onStart()
+	{
+		super.onStart();
+
+		Log.i(TAG, "Crown Activity started");
+	}
+
+//-----------------------------------------------------------------------------
+	public void onResume()
+	{
+		super.onResume();
+
+		// init accelerometer
+		mSensor.startListening(this);
+
+		Log.i(TAG, "Crown Activity resumed");
+	}
+
+//-----------------------------------------------------------------------------
+	public void onPause()
+	{
+		super.onPause();
+
+		// stop accelerometer
+		mSensor.stopListening();
+
+		Log.i(TAG, "Crown Activity paused");
+	}
+
+//-----------------------------------------------------------------------------
+	public void onStop()
+	{
+		super.onStop();
+
+		Log.i(TAG, "Crown Activity stopped");
+	}
+
+//-----------------------------------------------------------------------------
+	public void onRestart()
+	{
+		super.onRestart();
+
+		Log.i(TAG, "Crown Activity restarted");
+	}
+
+//-----------------------------------------------------------------------------
+	public void onDestroy()
+	{
+		super.onDestroy();
+
+		CrownLib.stopDevice();
+
+		Log.i(TAG, "Crown Activity destroyed");
+	}
+
+//-----------------------------------------------------------------------------
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		mTouch.onTouch(event);
+        return super.onTouchEvent(event);
+	}
+}
---
+++ engine/os/android/java/CrownTouch.java
@@ -0,0 +1,90 @@
+/*
+Copyright (c) 2013 Daniele Bartolini, Michele Rossi
+Copyright (c) 2012 Daniele Bartolini, Simone Boscaratto
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package crown.android;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
+
+
+/**
+* CrownTouch manages touch and gesture events passing them to Crown Engine
+*/
+
+//TODO: gestures doesn't work...fix them
+public class CrownTouch
+{
+	private boolean 				isListening;
+
+//-----------------------------------------------------------------------------------
+	public CrownTouch(Context context)
+	{
+		isListening = false;
+	}
+
+//-----------------------------------------------------------------------------------
+	public void onTouch(MotionEvent event)
+	{
+		final int pointerIndex = event.getActionIndex();
+		final int pointerCount = event.getPointerCount();
+
+		final int pointerId = event.getPointerId(pointerIndex);
+		final float x = event.getX(pointerIndex);
+		final float y = event.getY(pointerIndex);
+
+		final int actionMasked = event.getActionMasked();
+
+		switch (actionMasked) 
+		{	
+			case MotionEvent.ACTION_DOWN:
+			case MotionEvent.ACTION_POINTER_DOWN:
+			{
+				CrownLib.pushIntEvent(CrownEnum.OSET_TOUCH_DOWN, pointerId, (int)x, (int)y, 0);
+				break;			
+			}
+
+			case MotionEvent.ACTION_UP:
+			case MotionEvent.ACTION_POINTER_UP:
+			case MotionEvent.ACTION_OUTSIDE:
+			case MotionEvent.ACTION_CANCEL:
+			{
+				CrownLib.pushIntEvent(CrownEnum.OSET_TOUCH_UP, pointerId, (int)x, (int)y, 0);
+				break;			
+			}
+			
+			case MotionEvent.ACTION_MOVE:
+			{
+				for (int index = 0; index < pointerCount; index++)
+				{
+					CrownLib.pushIntEvent(CrownEnum.OSET_TOUCH_MOVE, event.getPointerId(index), (int)event.getX(index), (int)event.getY(index), 0);
+				}
+
+				break;
+			}
+		}
+	}
+}
