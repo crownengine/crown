f50f46d597e9248169047aa365d1cd467f20cd21
mikymod <mikymod@gmail.com>
2013-09-18 23:39:11 +0200

    add decoders test

---
+++ engine/tests/decoders.cpp
@@ -0,0 +1,27 @@
+#include "Crown.h"
+
+using namespace crown;
+
+int main(int argc, char** argv)
+{
+	Device* engine = device();
+
+	engine->init(argc, argv);
+
+	// Begin Test
+	ResourceId rid = engine->resource_manager()->load("sound", "sounds/untrue");
+	engine->resource_manager()->flush();
+
+	SoundResource* stream = (SoundResource*)engine->resource_manager()->data(rid);
+
+	OggDecoder decoder((char*)stream->data(), stream->size());
+
+	while(decoder.stream())
+	{
+		Log::i("size: %d", decoder.size());
+	}
+
+	engine->shutdown();
+
+	return 0;
+}
