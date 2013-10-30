f19b6f2b5dc373a286be40a14ad6c000c2178162
mikymod <mikymod@gmail.com>
2013-10-30 16:10:34 +0100

    update PackageResource according to Sprites implementation

--- engine/resource/PackageResource.h
+++ engine/resource/PackageResource.h
@@ -46,6 +46,8 @@ struct PackageHeader
 	uint32_t sounds_offset;
 	uint32_t num_meshes;
 	uint32_t meshes_offset;
+	uint32_t num_sprites;
+	uint32_t sprites_offset;
 };
 
 class PackageResource
@@ -121,6 +123,14 @@ public:
 	}
 
 	//-----------------------------------------------------------------------------
+	uint32_t num_sprites() const
+	{
+		CE_ASSERT_NOT_NULL(m_data);
+
+		return ((PackageHeader*)m_data)->num_sprites;
+	}
+
+	//-----------------------------------------------------------------------------
 	ResourceId get_texture_id(uint32_t i) const
 	{
 		CE_ASSERT(i < num_textures(), "Index out of bounds");
@@ -153,6 +163,15 @@ public:
 		CE_ASSERT(i < num_meshes(), "Index out of bounds");
 
 		ResourceId* begin = (ResourceId*) (m_data + ((PackageHeader*)m_data)->meshes_offset);
+		return begin[i];
+	}
+
+	//-----------------------------------------------------------------------------
+	ResourceId get_sprite_id(uint32_t i) const
+	{
+		CE_ASSERT(i < num_sprites(), "Index out of bounds");
+
+		ResourceId* begin = (ResourceId*) (m_data + ((PackageHeader*)m_data)->sprites_offset);
 		return begin[i];
 	}
 
