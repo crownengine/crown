1b8361edc413175a0890414f4cf50bbbef807876
mikymod <mikymod@gmail.com>
2013-11-20 09:56:30 +0100

    add Sound and SoundWorld

---
+++ engine/Sound.h
@@ -0,0 +1,72 @@
+/*
+Copyright (c) 2013 Daniele Bartolini, Michele Rossi
+Copyright (c) 2012 Daniele Bartolini, Simone Boscaratto
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include "SoundRenderer.h"
+#include "Matrix4x4.h"
+
+namespace crown
+{
+
+class SoundResource;
+class Vector3;
+class Quaternion;
+class Unit;
+
+struct Sound
+{
+	void			create(SoundResource* sr, int32_t node, const Vector3& pos, const Quaternion& dir);
+	void			destroy();
+
+	Vector3			local_position() const;
+	Quaternion		local_rotation() const;
+	Matrix4x4		local_pose() const;
+
+	Vector3			world_position() const;
+	Quaternion		world_rotation() const;
+	Matrix4x4		world_pose() const;
+
+	void			set_local_position(Unit* unit, const Vector3& pos);
+	void			set_local_rotation(Unit* unit, const Quaternion& rot);
+	void			set_local_pose(Unit* unit, const Matrix4x4& pose);
+
+public:
+
+	uint32_t		m_node;
+	Matrix4x4		m_local_pose;
+	Matrix4x4		m_world_pose;
+
+	float			m_volume;
+	float			m_range;
+	bool			m_loop : 1;
+	bool			m_playing : 1;
+
+	SoundBufferId	m_buffer;
+	SoundSourceId	m_source;
+};
+
+} // namespace crown
