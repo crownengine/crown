96e371ec091c0a7570d5214c8259cbeb2e13c529
mikymod <mikymod@gmail.com>
2014-01-17 17:27:15 +0100

    clean Unit

--- engine/world/Unit.cpp
+++ engine/world/Unit.cpp
@@ -206,8 +206,9 @@ void Unit::create_physics_objects()
 		// Create actors if any
 		for (uint32_t i = 0; i < pr->num_actors(); i++)
 		{
-			const PhysicsActor actor = pr->actor(i);
-			add_actor(actor.name, m_world.physics_world()->create_actor(actor, m_scene_graph, m_scene_graph.node(actor.node)));
+			const PhysicsActor& actor = pr->actor(i);
+
+			add_actor(actor.name, m_world.physics_world()->create_actor(pr, i, m_scene_graph, m_scene_graph.node(actor.node)));
 		}
 	}
 }
