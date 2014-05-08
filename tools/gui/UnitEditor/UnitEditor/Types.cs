917c28fe6ef9d00278c592d5b4c95e28ec394abe
mikymod <mikymod@gmail.com>
2014-05-08 12:40:02 +0200

    add physics types

--- tools/gui/UnitEditor/UnitEditor/Types.cs
+++ tools/gui/UnitEditor/UnitEditor/Types.cs
@@ -2,35 +2,52 @@ using System.Collections.Generic;
 
 namespace UnitEditor
 {
-	
-//------------------------------------------------------------------------------
-public class Renderable
-{
-	public Renderable(string node, string type, string resource, bool visible)
+	//------------------------------------------------------------------------------
+	public class Renderable
 	{
-		this.node = node;
-		this.type = type;
-		this.resource = resource;
-		this.visible = visible;
+		public string node;
+		public string type;
+		public string resource;
+		public bool visible;
 	}
 
-	public string node { set; get; }
-	public string type { set; get; }
-	public string resource { set; get; }
-	public bool visible { set; get; }
-
-	public override string ToString()
+	//------------------------------------------------------------------------------
+	public class Node
 	{
-		return "node: " + node + ", type: " + type + ", resource: " + resource + ", visible: " + visible + "\n";
+		public string parent;
+		public float[] position = new float[3];
+		public float[] rotation = new float[4];
 	}
-}
 
-//------------------------------------------------------------------------------
-public class Node
-{
-	public string parent { set; get; }
-	public float[] position = new float[3];
-	public float[] rotation = new float[4];
-}
+	//------------------------------------------------------------------------------
+	public class Actor
+	{
+		public string name;
+		public string node;
+		public string actor_class;
+		public float mass;
+	}
+
+	//------------------------------------------------------------------------------
+	public class Shape
+	{
+		public string type;
+		public string shape_class;
+		public string material;
+		public float data_0;
+		public float data_1;
+		public float data_2;
+		public float data_3;
+	}
+
+	//------------------------------------------------------------------------------
+	public class Joint
+	{
+		public string type;
+		public string actor_0;
+		public float[] anchor_0 = new float[3];
+		public string actor_1;
+		public float[] anchor_1 = new float[3];
+	}
 
 } // namespace UnitEditor
