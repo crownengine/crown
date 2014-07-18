4fc5980ed4df224289a2e478cbbf410c3d602b8b
mikymod <mikymod@gmail.com>
2014-07-18 21:58:30 +0200

    Update some #ifdefs

--- engine/core/Error.h
+++ engine/core/Error.h
@@ -33,7 +33,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 #include <stdio.h>
 #include <stdarg.h>
 
-#if defined __GNUG__ && defined(LINUX)
+#if defined(CROWN_COMPILER_GCC) && defined(CROWN_PLATFORM_LINUX)
 	#include <cxxabi.h>
 	#include <execinfo.h>
 #endif
@@ -45,7 +45,7 @@ namespace error
 
 inline void log_backtrace()
 {
-	#if defined __GNUG__ && defined(LINUX)
+	#if defined(CROWN_COMPILER_GCC) && defined(CROWN_PLATFORM_LINUX)
 	void* array[50];
 	int size = backtrace(array, 50);
 
