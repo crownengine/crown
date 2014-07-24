89100b2ef096081101646218641080f1e9779d78
mikymod <mikymod@gmail.com>
2014-07-24 16:00:38 +0200

    add starter gui

---
+++ tools/gui/starter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace starter
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
