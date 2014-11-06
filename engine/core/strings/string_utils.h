0d80b71f39c452709ceb050c57a1735ac5fe5cb6
Daniele Bartolini <danyatk@gmail.com>
2014-11-06 09:04:09 +0100

    Cleanup

--- engine/core/strings/string_utils.h
+++ engine/core/strings/string_utils.h
@@ -37,31 +37,6 @@ OTHER DEALINGS IN THE SOFTWARE.
 namespace crown
 {
 
-inline bool is_alpha(char c)
-{
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
-
-inline bool is_digit(char c)
-{
-	return !(c < '0' || c > '9');
-}
-
-inline bool is_upper(char c)
-{
-	return (c >= 'A' && c <= 'Z');
-}
-
-inline bool is_lower(char c)
-{
-	return (c >= 'a' && c <= 'z');
-}
-
-inline bool is_whitespace(char c)
-{
-	return (c == ' ' || c == '\t' || c == '\n' || c == '\r');
-}
-
 inline size_t strlen(const char* str)
 {
 	return ::strlen(str);
