4fdacb3c0d56ad632ccd2c5c853657619db45074
Daniele Bartolini <danyatk@gmail.com>
2014-12-16 01:14:26 +0100

    Switch to NJSON

--- engine/core/json/json_parser.cpp
+++ engine/core/json/json_parser.cpp
@@ -4,7 +4,7 @@
  */
 
 #include "json_parser.h"
-#include "json.h"
+#include "njson.h"
 #include "temp_allocator.h"
 #include "string_utils.h"
 #include "vector.h"
@@ -45,7 +45,7 @@ JSONElement JSONElement::operator[](uint32_t i)
 {
 	Array<const char*> array(default_allocator());
 
-	json::parse_array(_at, array);
+	njson::parse_array(_at, array);
 
 	CE_ASSERT(i < array::size(array), "Index out of bounds");
 
@@ -63,7 +63,7 @@ JSONElement JSONElement::index_or_nil(uint32_t i)
 	{
 		Array<const char*> array(default_allocator());
 
-		json::parse_array(_at, array);
+		njson::parse_array(_at, array);
 
 		if (i >= array::size(array))
 		{
@@ -79,7 +79,7 @@ JSONElement JSONElement::index_or_nil(uint32_t i)
 JSONElement JSONElement::key(const char* k)
 {
 	Map<DynamicString, const char*> object(default_allocator());
-	json::parse_object(_at, object);
+	njson::parse_object(_at, object);
 
 	const char* value = map::get(object, DynamicString(k), (const char*) NULL);
 	CE_ASSERT(value != NULL, "Key not found: '%s'", k);
@@ -92,7 +92,7 @@ JSONElement JSONElement::key_or_nil(const char* k)
 	if (_at != NULL)
 	{
 		Map<DynamicString, const char*> object(default_allocator());
-		json::parse_object(_at, object);
+		njson::parse_object(_at, object);
 
 		const char* value = map::get(object, DynamicString(k), (const char*) NULL);
 
@@ -106,24 +106,24 @@ JSONElement JSONElement::key_or_nil(const char* k)
 bool JSONElement::has_key(const char* k) const
 {
 	Map<DynamicString, const char*> object(default_allocator());
-	json::parse_object(_at, object);
+	njson::parse_object(_at, object);
 
 	return map::has(object, DynamicString(k));
 }
 
 bool JSONElement::to_bool(bool def) const
 {
-	return is_nil() ? def : json::parse_bool(_at);
+	return is_nil() ? def : njson::parse_bool(_at);
 }
 
 int32_t JSONElement::to_int(int32_t def) const
 {
-	return is_nil() ? def : json::parse_int(_at);
+	return is_nil() ? def : njson::parse_int(_at);
 }
 
 float JSONElement::to_float(float def) const
 {
-	return is_nil() ? def : json::parse_float(_at);
+	return is_nil() ? def : njson::parse_float(_at);
 }
 
 void JSONElement::to_string(DynamicString& str, const char* def) const
@@ -131,7 +131,7 @@ void JSONElement::to_string(DynamicString& str, const char* def) const
 	if (is_nil())
 		str = def;
 	else
-		json::parse_string(_at, str);
+		njson::parse_string(_at, str);
 }
 
 Vector2 JSONElement::to_vector2(const Vector2& def) const
@@ -141,10 +141,10 @@ Vector2 JSONElement::to_vector2(const Vector2& def) const
 
 	TempAllocator64 alloc;
 	Array<const char*> array(alloc);
-	json::parse_array(_at, array);
+	njson::parse_array(_at, array);
 
-	return Vector2(json::parse_float(array[0]),
-					json::parse_float(array[1]));
+	return Vector2(njson::parse_float(array[0]),
+					njson::parse_float(array[1]));
 }
 
 Vector3 JSONElement::to_vector3(const Vector3& def) const
@@ -154,11 +154,11 @@ Vector3 JSONElement::to_vector3(const Vector3& def) const
 
 	TempAllocator64 alloc;
 	Array<const char*> array(alloc);
-	json::parse_array(_at, array);
+	njson::parse_array(_at, array);
 
-	return Vector3(json::parse_float(array[0]),
-					json::parse_float(array[1]),
-					json::parse_float(array[2]));
+	return Vector3(njson::parse_float(array[0]),
+					njson::parse_float(array[1]),
+					njson::parse_float(array[2]));
 }
 
 Vector4 JSONElement::to_vector4(const Vector4& def) const
@@ -168,12 +168,12 @@ Vector4 JSONElement::to_vector4(const Vector4& def) const
 
 	TempAllocator64 alloc;
 	Array<const char*> array(alloc);
-	json::parse_array(_at, array);
+	njson::parse_array(_at, array);
 
-	return Vector4(json::parse_float(array[0]),
-					json::parse_float(array[1]),
-					json::parse_float(array[2]),
-					json::parse_float(array[3]));
+	return Vector4(njson::parse_float(array[0]),
+					njson::parse_float(array[1]),
+					njson::parse_float(array[2]),
+					njson::parse_float(array[3]));
 }
 
 Quaternion JSONElement::to_quaternion(const Quaternion& def) const
@@ -183,12 +183,12 @@ Quaternion JSONElement::to_quaternion(const Quaternion& def) const
 
 	TempAllocator64 alloc;
 	Array<const char*> array(alloc);
-	json::parse_array(_at, array);
+	njson::parse_array(_at, array);
 
-	return Quaternion(json::parse_float(array[0]),
-					json::parse_float(array[1]),
-					json::parse_float(array[2]),
-					json::parse_float(array[3]));
+	return Quaternion(njson::parse_float(array[0]),
+					njson::parse_float(array[1]),
+					njson::parse_float(array[2]),
+					njson::parse_float(array[3]));
 }
 
 Matrix4x4 JSONElement::to_matrix4x4(const Matrix4x4& def) const
@@ -210,7 +210,7 @@ StringId32 JSONElement::to_string_id(const StringId32 def) const
 
 	TempAllocator1024 alloc;
 	DynamicString str(alloc);
-	json::parse_string(_at, str);
+	njson::parse_string(_at, str);
 	return str.to_string_id();
 }
 
@@ -219,7 +219,7 @@ ResourceId JSONElement::to_resource_id(const char* type) const
 	CE_ASSERT_NOT_NULL(type);
 	// TempAllocator1024 alloc;
 	DynamicString str(default_allocator());
-	json::parse_string(_at, str);
+	njson::parse_string(_at, str);
 	return ResourceId(type, str.c_str());
 }
 
@@ -227,11 +227,11 @@ void JSONElement::to_array(Array<bool>& array) const
 {
 	Array<const char*> temp(default_allocator());
 
-	json::parse_array(_at, temp);
+	njson::parse_array(_at, temp);
 
 	for (uint32_t i = 0; i < array::size(temp); i++)
 	{
-		array::push_back(array, json::parse_bool(temp[i]));
+		array::push_back(array, njson::parse_bool(temp[i]));
 	}
 }
 
@@ -239,11 +239,11 @@ void JSONElement::to_array(Array<int16_t>& array) const
 {
 	Array<const char*> temp(default_allocator());
 
-	json::parse_array(_at, temp);
+	njson::parse_array(_at, temp);
 
 	for (uint32_t i = 0; i < array::size(temp); i++)
 	{
-		array::push_back(array, (int16_t)json::parse_int(temp[i]));
+		array::push_back(array, (int16_t)njson::parse_int(temp[i]));
 	}
 }
 
@@ -251,11 +251,11 @@ void JSONElement::to_array(Array<uint16_t>& array) const
 {
 	Array<const char*> temp(default_allocator());
 
-	json::parse_array(_at, temp);
+	njson::parse_array(_at, temp);
 
 	for (uint32_t i = 0; i < array::size(temp); i++)
 	{
-		array::push_back(array, (uint16_t)json::parse_int(temp[i]));
+		array::push_back(array, (uint16_t)njson::parse_int(temp[i]));
 	}
 }
 
@@ -263,11 +263,11 @@ void JSONElement::to_array(Array<int32_t>& array) const
 {
 	Array<const char*> temp(default_allocator());
 
-	json::parse_array(_at, temp);
+	njson::parse_array(_at, temp);
 
 	for (uint32_t i = 0; i < array::size(temp); i++)
 	{
-		array::push_back(array, (int32_t)json::parse_int(temp[i]));
+		array::push_back(array, (int32_t)njson::parse_int(temp[i]));
 	}
 }
 
@@ -275,11 +275,11 @@ void JSONElement::to_array(Array<uint32_t>& array) const
 {
 	Array<const char*> temp(default_allocator());
 
-	json::parse_array(_at, temp);
+	njson::parse_array(_at, temp);
 
 	for (uint32_t i = 0; i < array::size(temp); i++)
 	{
-		array::push_back(array, (uint32_t)json::parse_int(temp[i]));
+		array::push_back(array, (uint32_t)njson::parse_int(temp[i]));
 	}
 }
 
@@ -287,11 +287,11 @@ void JSONElement::to_array(Array<float>& array) const
 {
 	Array<const char*> temp(default_allocator());
 
-	json::parse_array(_at, temp);
+	njson::parse_array(_at, temp);
 
 	for (uint32_t i = 0; i < array::size(temp); i++)
 	{
-		array::push_back(array, json::parse_float(temp[i]));
+		array::push_back(array, njson::parse_float(temp[i]));
 	}
 }
 
@@ -299,12 +299,12 @@ void JSONElement::to_array(Vector<DynamicString>& array) const
 {
 	Array<const char*> temp(default_allocator());
 
-	json::parse_array(_at, temp);
+	njson::parse_array(_at, temp);
 
 	for (uint32_t i = 0; i < array::size(temp); i++)
 	{
 		DynamicString str;
-		json::parse_string(temp[i], str);
+		njson::parse_string(temp[i], str);
 		vector::push_back(array, str);
 	}
 }
@@ -312,7 +312,7 @@ void JSONElement::to_array(Vector<DynamicString>& array) const
 void JSONElement::to_keys(Vector<DynamicString>& keys) const
 {
 	Map<DynamicString, const char*> object(default_allocator());
-	json::parse_object(_at, object);
+	njson::parse_object(_at, object);
 
 	const Map<DynamicString, const char*>::Node* it = map::begin(object);
 	while (it != map::end(object))
@@ -326,7 +326,7 @@ bool JSONElement::is_nil() const
 {
 	if (_at != NULL)
 	{
-		return json::type(_at) == JSONType::NIL;
+		return njson::type(_at) == NJSONType::NIL;
 	}
 
 	return true;
@@ -336,7 +336,7 @@ bool JSONElement::is_bool() const
 {
 	if (_at != NULL)
 	{
-		return json::type(_at) == JSONType::BOOL;
+		return njson::type(_at) == NJSONType::BOOL;
 	}
 
 	return false;
@@ -346,7 +346,7 @@ bool JSONElement::is_number() const
 {
 	if (_at != NULL)
 	{
-		return json::type(_at) == JSONType::NUMBER;
+		return njson::type(_at) == NJSONType::NUMBER;
 	}
 
 	return false;
@@ -356,7 +356,7 @@ bool JSONElement::is_string() const
 {
 	if (_at != NULL)
 	{
-		return json::type(_at) == JSONType::STRING;
+		return njson::type(_at) == NJSONType::STRING;
 	}
 
 	return false;
@@ -366,7 +366,7 @@ bool JSONElement::is_array() const
 {
 	if (_at != NULL)
 	{
-		return json::type(_at) == JSONType::ARRAY;
+		return njson::type(_at) == NJSONType::ARRAY;
 	}
 
 	return false;
@@ -376,7 +376,7 @@ bool JSONElement::is_object() const
 {
 	if (_at != NULL)
 	{
-		return json::type(_at) == JSONType::OBJECT;
+		return njson::type(_at) == NJSONType::OBJECT;
 	}
 
 	return false;
@@ -389,35 +389,35 @@ uint32_t JSONElement::size() const
 		return 0;
 	}
 
-	switch(json::type(_at))
+	switch(njson::type(_at))
 	{
-		case JSONType::NIL:
+		case NJSONType::NIL:
 		{
 			return 1;
 		}
-		case JSONType::OBJECT:
+		case NJSONType::OBJECT:
 		{
 			Map<DynamicString, const char*> object(default_allocator());
-			json::parse_object(_at, object);
+			njson::parse_object(_at, object);
 			return map::size(object);
 		}
-		case JSONType::ARRAY:
+		case NJSONType::ARRAY:
 		{
 			Array<const char*> array(default_allocator());
-			json::parse_array(_at, array);
+			njson::parse_array(_at, array);
 			return array::size(array);
 		}
-		case JSONType::STRING:
+		case NJSONType::STRING:
 		{
 			DynamicString string;
-			json::parse_string(_at, string);
+			njson::parse_string(_at, string);
 			return string.length();
 		}
-		case JSONType::NUMBER:
+		case NJSONType::NUMBER:
 		{
 			return 1;
 		}
-		case JSONType::BOOL:
+		case NJSONType::BOOL:
 		{
 			return 1;
 		}
