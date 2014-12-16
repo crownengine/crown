de9a4d939f4080a1830c248541242b6de9e82cb5
Daniele Bartolini <danyatk@gmail.com>
2014-12-16 23:24:35 +0100

    Update samples

--- samples/01.hello-world/lua/game.lua
+++ samples/01.hello-world/lua/game.lua
@@ -1,9 +1,21 @@
+world = world or nil
+camera = camera or nil
+
 function init()
 	-- Set the title of the main window
 	Window.set_title("Hello world!")
+
+	-- Create world and camera
+	world = Device.create_world()
+	local camera_unit = World.spawn_unit(world, "camera")
+	camera = Unit.camera(camera_unit, "camera")
+	Camera.set_orthographic_metrics(camera, 0, 0, 1280, 720)
 end
 
 function update(dt)
+	-- Advance the simulation
+	World.update(world, dt)
+
 	-- Stop the engine when the 'ESC' key is released
 	if Keyboard.button_released(Keyboard.ESCAPE) then
 		Device.quit()
@@ -11,8 +23,15 @@ function update(dt)
 end
 
 function render(dt)
-	-- Render nothing
+	-- Render the world
+	Device.render_world(world, camera)
 end
 
 function shutdown()
+	-- Cleanup
+	Device.destroy_world(world)
+end
+
+function g_physics_callback(args)
+	-- Do nothing
 end
