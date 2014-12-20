4db46a7e11951a49b4eabcf75ab37ca3882af071
Daniele Bartolini <danyatk@gmail.com>
2014-12-20 07:34:08 +0100

    Cleanup

--- engine/core/math/math_types.h
+++ engine/core/math/math_types.h
@@ -118,9 +118,9 @@ struct Matrix4x4
 	Matrix4x4(const Matrix3x3& m);
 
 	Matrix4x4(float r1c1, float r2c1, float r3c1, float r4c1,
-				float r1c2, float r2c2, float r3c2, float r4c2,
-				float r1c3, float r2c3, float r3c3, float r4c3,
-				float r1c4, float r2c4, float r3c4, float r4c4);
+		float r1c2, float r2c2, float r3c2, float r4c2,
+		float r1c3, float r2c3, float r3c3, float r4c3,
+		float r1c4, float r2c4, float r3c4, float r4c4);
 
 	Matrix4x4(const float v[16]);
 
