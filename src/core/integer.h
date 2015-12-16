a090dae264b65b9e9eba9943d036c096f5afad34
Daniele Bartolini <danyatk@gmail.com>
2015-12-16 01:56:16 +0100

    Add integer.h

---
+++ src/core/integer.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2012-2015 Daniele Bartolini and individual contributors.
+ * License: https://github.com/taylor001/crown/blob/master/LICENSE
+ */
+
+#pragma once
+
+#include "types.h"
+
+namespace crown
+{
+
+inline uint32_t next_pow_2(uint32_t x)
+{
+	x--;
+
+	x = (x >> 1) | x;
+	x = (x >> 2) | x;
+	x = (x >> 4) | x;
+	x = (x >> 8) | x;
+	x = (x >> 16) | x;
+
+	return ++x;
+}
+
+inline bool is_pow_2(uint32_t x)
+{
+	return !(x & (x - 1)) && x;
+}
+
+} // namespace crown
