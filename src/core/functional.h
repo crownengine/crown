e9a16393e9482210286f43b46989c3c9aa6d373f
Daniele Bartolini <danyatk@gmail.com>
2015-12-17 17:00:44 +0100

    Add missing comparison operations

--- src/core/functional.h
+++ src/core/functional.h
@@ -9,11 +9,20 @@ namespace crown
 {
 
 template<typename T>
-struct less
+struct equal_to
 {
 	bool operator()(const T& a, const T& b) const
 	{
-		return a < b;
+		return a == b;
+	};
+};
+
+template<typename T>
+struct not_equal_to
+{
+	bool operator()(const T& a, const T& b) const
+	{
+		return a != b;
 	};
 };
 
@@ -23,6 +32,33 @@ struct greater
 	bool operator()(const T& a, const T& b) const
 	{
 		return a > b;
+	};
+};
+
+template<typename T>
+struct less
+{
+	bool operator()(const T& a, const T& b) const
+	{
+		return a < b;
+	};
+};
+
+template<typename T>
+struct greater_equal
+{
+	bool operator()(const T& a, const T& b) const
+	{
+		return a >= b;
+	};
+};
+
+template<typename T>
+struct less_equal
+{
+	bool operator()(const T& a, const T& b) const
+	{
+		return a <= b;
 	};
 };
 
