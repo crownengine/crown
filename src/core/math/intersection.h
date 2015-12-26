3995bc0db64e23835dc704db9baed5211da1db09
Daniele Bartolini <danyatk@gmail.com>
2015-12-26 11:40:28 +0100

    Cleanup

--- src/core/math/intersection.h
+++ src/core/math/intersection.h
@@ -28,8 +28,13 @@ float ray_sphere_intersection(const Vector3& from, const Vector3& dir, const Sph
 /// bounding box (tm, half_extents) or -1.0 if no intersection.
 float ray_obb_intersection(const Vector3& from, const Vector3& dir, const Matrix4x4& tm, const Vector3& half_extents);
 
-bool plane_3_intersection(const Plane& p1, const Plane& p2, const Plane& p3, Vector3& ip);
+/// Returns whether the planes @a a, @a b and @a c intersects and if so fills @a ip with the intersection point.
+bool plane_3_intersection(const Plane& a, const Plane& b, const Plane& c, Vector3& ip);
+
+/// Returns whether the frustum @a f and the sphere @a s intersects.
 bool frustum_sphere_intersection(const Frustum& f, const Sphere& s);
+
+/// Returns whether the frustum @a f and the AABB @a b intersects.
 bool frustum_box_intersection(const Frustum& f, const AABB& b);
 
 /// @}
