f8f566c81770702d4b1b693dbbf05217d656c889
Daniele Bartolini <danyatk@gmail.com>
2015-12-29 10:41:48 +0100

    Improve InputDevice

--- src/input/input_device.h
+++ src/input/input_device.h
@@ -8,6 +8,7 @@
 #include "types.h"
 #include "math_types.h"
 #include "memory_types.h"
+#include "string_id.h"
 
 namespace crown
 {
@@ -29,11 +30,11 @@ struct InputDevice
 	/// Returns the number of axes of the input devices.
 	uint8_t num_axes() const;
 
-	/// Returns whether the specified @a b button is pressed in the current frame.
-	bool pressed(uint8_t i) const;
+	/// Returns whether the button @a id is pressed in the current frame.
+	bool pressed(uint8_t id) const;
 
-	/// Returns whether the specified @a b button is released in the current frame.
-	bool released(uint8_t i) const;
+	/// Returns whether the button @a id is released in the current frame.
+	bool released(uint8_t id) const;
 
 	/// Returns whether any button is pressed in the current frame.
 	bool any_pressed() const;
@@ -41,8 +42,20 @@ struct InputDevice
 	/// Returns whether any button is released in the current frame.
 	bool any_released() const;
 
-	/// Returns the value of the axis @a i.
-	Vector3 axis(uint8_t i) const;
+	/// Returns the value of the axis @a id.
+	Vector3 axis(uint8_t id) const;
+
+	/// Returns the name of the button @a id.
+	const char* button_name(uint8_t id);
+
+	/// Returns the name of the axis @a id.
+	const char* axis_name(uint8_t id);
+
+	/// Returns the id of the button @a name.
+	uint8_t button_id(StringId32 name);
+
+	/// Returns the id of the axis @a name.
+	uint8_t axis_id(StringId32 name);
 
 	void set_connected(bool connected);
 
@@ -59,14 +72,18 @@ public:
 	uint8_t _num_axes;
 	uint8_t _last_button;
 
-	uint8_t* _last_state;    // num_buttons
-	uint8_t* _current_state; // num_buttons
-	Vector3* _axis;          // num_axes
-	char* _name;             // strlen32(name) + 1
+	uint8_t* _last_state;      // num_buttons
+	uint8_t* _current_state;   // num_buttons
+	Vector3* _axis;            // num_axes
+	const char** _button_name; // num_buttons
+	const char** _axis_name;   // num_buttons
+	StringId32* _button_hash;  // num_buttons
+	StringId32* _axis_hash;    // num_buttons
+	char* _name;               // strlen32(name) + 1
 
 public:
 
-	static InputDevice* create(Allocator& a, const char* name, uint8_t num_buttons, uint8_t num_axes);
+	static InputDevice* create(Allocator& a, const char* name, uint8_t num_buttons, uint8_t num_axes, const char** button_names, const char** axis_names);
 	static void destroy(Allocator& a, InputDevice* id);
 };
 
