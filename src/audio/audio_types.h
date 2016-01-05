dad74f664e5a031087fbc8acd646e9e911039813
Daniele Bartolini <danyatk@gmail.com>
2016-01-05 20:35:22 +0100

    Add audio_types.h

---
+++ src/audio/audio_types.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2012-2016 Daniele Bartolini and individual contributors.
+ * License: https://github.com/taylor001/crown/blob/master/LICENSE
+ */
+
+#pragma once
+
+namespace crown
+{
+
+class SoundWorld;
+typedef uint32_t SoundInstanceId;
+
+} // namespace crown
