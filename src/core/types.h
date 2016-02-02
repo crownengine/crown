00607a62f03e437686f21d1ad3467096c7f03c3f
Daniele Bartolini <danyatk@gmail.com>
2016-02-02 09:28:11 +0100

    Fix windows build

--- src/core/types.h
+++ src/core/types.h
@@ -18,3 +18,11 @@ typedef int64_t  s64;
 typedef uint64_t u64;
 typedef float    f32;
 typedef double   f64;
+
+#if defined(_MSC_VER)
+	#define _ALLOW_KEYWORD_MACROS
+#endif
+
+#if !defined(alignof)
+	#define alignof(x) __alignof(x)
+#endif
