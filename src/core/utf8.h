723422f7dfb8b8f583d0bbbc3d1daca64755a751
Daniele Bartolini <danyatk@gmail.com>
2016-02-04 22:07:52 +0100

    Add utf8::decode()

---
+++ src/core/utf8.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2012-2016 Daniele Bartolini and individual contributors.
+ * License: https://github.com/taylor001/crown/blob/master/LICENSE
+ */
+
+#pragma once
+
+#include "types.h"
+
+#define UTF8_ACCEPT 0
+#define UTF8_REJECT 1
+
+namespace crown
+{
+namespace utf8
+{
+	/// See http://bjoern.hoehrmann.de/utf-8/decoder/dfa/ for details.
+	u32 decode(u32* state, u32* codep, u32 byte);
+
+} // namespace utf8
+} // namespace crown
