e999d749f9c8e107c89c8c55516f68cb79d522f0
Daniele Bartolini <danyatk@gmail.com>
2016-02-04 09:28:45 +0100

    Add some useful constants

--- src/core/math/math_types.h
+++ src/core/math/math_types.h
@@ -92,20 +92,25 @@ struct Sphere
 	f32 r;
 };
 
-const Vector2 VECTOR2_ZERO = { 0.0f, 0.0f };
+const Vector2 VECTOR2_ZERO  = { 0.0f, 0.0f };
+const Vector2 VECTOR2_ONE   = { 1.0f, 1.0f };
+const Vector2 VECTOR2_XAXIS = { 1.0f, 0.0f };
+const Vector2 VECTOR2_YAXIS = { 0.0f, 1.0f };
 
 const Vector3 VECTOR3_ZERO     = {  0.0f,  0.0f,  0.0f };
+const Vector3 VECTOR3_ONE      = {  1.0f,  1.0f,  1.0f };
 const Vector3 VECTOR3_XAXIS    = {  1.0f,  0.0f,  0.0f };
 const Vector3 VECTOR3_YAXIS    = {  0.0f,  1.0f,  0.0f };
 const Vector3 VECTOR3_ZAXIS    = {  0.0f,  0.0f,  1.0f };
-const Vector3 VECTOR3_FORWARD  = {  0.0f,  0.0f,  1.0f };
-const Vector3 VECTOR3_BACKWARD = {  0.0f,  0.0f, -1.0f };
-const Vector3 VECTOR3_LEFT     = { -1.0f,  0.0f,  0.0f };
 const Vector3 VECTOR3_RIGHT    = {  1.0f,  0.0f,  0.0f };
+const Vector3 VECTOR3_LEFT     = { -1.0f,  0.0f,  0.0f };
 const Vector3 VECTOR3_UP       = {  0.0f,  1.0f,  0.0f };
 const Vector3 VECTOR3_DOWN     = {  0.0f, -1.0f,  0.0f };
+const Vector3 VECTOR3_FORWARD  = {  0.0f,  0.0f,  1.0f };
+const Vector3 VECTOR3_BACKWARD = {  0.0f,  0.0f, -1.0f };
 
 const Vector4 VECTOR4_ZERO  = { 0.0f, 0.0f, 0.0f, 0.0f };
+const Vector4 VECTOR4_ONE   = { 1.0f, 1.0f, 1.0f, 1.0f };
 const Vector4 VECTOR4_XAXIS = { 1.0f, 0.0f, 0.0f, 0.0f };
 const Vector4 VECTOR4_YAXIS = { 0.0f, 1.0f, 0.0f, 0.0f };
 const Vector4 VECTOR4_ZAXIS = { 0.0f, 0.0f, 1.0f, 0.0f };
