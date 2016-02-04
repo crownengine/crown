0a023c9fccf3509b9a571f3b9a77a65c3edf574a
Daniele Bartolini <danyatk@gmail.com>
2016-02-04 15:06:08 +0100

    Use colored output where available

--- src/core/log.cpp
+++ src/core/log.cpp
@@ -3,11 +3,12 @@
  * License: https://github.com/taylor001/crown/blob/master/LICENSE
  */
 
-#include "log.h"
 #include "console_server.h"
-#include "string_utils.h"
+#include "log.h"
 #include "os.h"
+#include "platform.h"
 #include "string_stream.h"
+#include "string_utils.h"
 
 namespace crown
 {
@@ -27,20 +28,20 @@ namespace log_internal
 		return ss;
 	}
 
-	static void console_log(const char* msg, LogSeverity::Enum severity)
+	static void console_log(const char* msg, LogSeverity::Enum sev)
 	{
 		if (!console_server_globals::console())
 			return;
 
-		using namespace string_stream;
 		static const char* stt[] = { "info", "warning", "error", "debug" };
 
 		// Build json message
+		using namespace string_stream;
 		TempAllocator2048 alloc;
 		StringStream json(alloc);
 
 		json << "{\"type\":\"message\",";
-		json << "\"severity\":\"" << stt[severity] << "\",";
+		json << "\"severity\":\"" << stt[sev] << "\",";
 		json << "\"message\":\""; sanitize(json, msg) << "\"}";
 
 		console_server_globals::console()->send(c_str(json));
@@ -48,11 +49,28 @@ namespace log_internal
 
 	void logx(LogSeverity::Enum sev, const char* msg, va_list args)
 	{
-		char buf[2048];
-		int len = vsnprintf(buf, sizeof(buf), msg, args);
-		if (len > (int)sizeof(buf))
-			len = sizeof(buf) - 1;
+		char buf[8192];
+		int len = 0;
 
+#if CROWN_PLATFORM_POSIX
+		#define ANSI_RESET  "\x1b[0m"
+		#define ANSI_YELLOW "\x1b[33m"
+		#define ANSI_RED    "\x1b[31m"
+
+		static const char* stt[] =
+		{
+			ANSI_RESET,
+			ANSI_YELLOW,
+			ANSI_RED,
+			ANSI_RESET
+		};
+
+		len +=  snprintf(&buf[len], sizeof(buf) - len, stt[sev]);
+		len += vsnprintf(&buf[len], sizeof(buf) - len, msg, args);
+		len +=  snprintf(&buf[len], sizeof(buf) - len, ANSI_RESET);
+#else
+		len = vsnprintf(&buf[len], sizeof(buf) - len, msg, args);
+#endif
 		buf[len] = '\0';
 		console_log(buf, sev);
 		os::log(buf);
