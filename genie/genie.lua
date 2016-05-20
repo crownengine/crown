c5776f7e8b1531b4486e624b5fe4c4de01510b9e
Daniele Bartolini <danyatk@gmail.com>
2016-05-20 18:46:16 +0200

    Put engine in engine folder

--- genie/genie.lua
+++ genie/genie.lua
@@ -4,8 +4,9 @@
 --
 
 CROWN_DIR = (path.getabsolute("..") .. "/")
-local CROWN_THIRD_DIR = (CROWN_DIR .. "third/")
-local CROWN_BUILD_DIR = (CROWN_DIR .. "build/")
+local CROWN_THIRD_DIR  = (CROWN_DIR .. "third/")
+local CROWN_BUILD_DIR  = (CROWN_DIR .. "build/")
+local CROWN_ENGINE_DIR = (CROWN_BUILD_DIR .. "engine/")
 BGFX_DIR = (CROWN_DIR .. "third/bgfx/")
 BX_DIR = (CROWN_DIR .. "third/bx/")
 
@@ -58,7 +59,7 @@ dofile ("toolchain.lua")
 dofile (BGFX_DIR .. "scripts/bgfx.lua")
 dofile ("crown.lua")
 
-toolchain(CROWN_BUILD_DIR, CROWN_THIRD_DIR)
+toolchain(CROWN_ENGINE_DIR, CROWN_THIRD_DIR)
 
 group "libs"
 bgfxProject("", "StaticLib", os.is("windows") and { "BGFX_CONFIG_RENDERER_DIRECT3D9=1" } or {})
@@ -83,56 +84,56 @@ end
 -- Install
 configuration { "x32", "linux-*" }
 	postbuildcommands {
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/linux_x86/luajit " .. CROWN_BUILD_DIR .. "linux32/bin",
-		"cp -r " .. CROWN_THIRD_DIR .. "luajit/src/jit "              .. CROWN_BUILD_DIR .. "linux32/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/linux_x86/luajit " .. CROWN_ENGINE_DIR .. "linux32/bin",
+		"cp -r " .. CROWN_THIRD_DIR .. "luajit/src/jit "              .. CROWN_ENGINE_DIR .. "linux32/bin",
 	}
 
 	if _OPTIONS["with-physx"] then
 		postbuildcommands {
-			"cp -r " .. "$(PHYSX_SDK_LINUX)/Bin/linux32/libPhysX3* " .. CROWN_BUILD_DIR .. "linux32/bin",
+			"cp -r " .. "$(PHYSX_SDK_LINUX)/Bin/linux32/libPhysX3* " .. CROWN_ENGINE_DIR .. "linux32/bin",
 		}
 	end
 
 configuration { "x64", "linux-*" }
 	postbuildcommands {
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/linux_x64/luajit " .. CROWN_BUILD_DIR .. "linux64/bin",
-		"cp -r " .. CROWN_THIRD_DIR .. "luajit/src/jit "              .. CROWN_BUILD_DIR .. "linux64/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/linux_x64/luajit " .. CROWN_ENGINE_DIR .. "linux64/bin",
+		"cp -r " .. CROWN_THIRD_DIR .. "luajit/src/jit "              .. CROWN_ENGINE_DIR .. "linux64/bin",
 	}
 
 	if _OPTIONS["with-physx"] then
 		postbuildcommands {
-			"cp -r " .. "$(PHYSX_SDK_LINUX)/Bin/linux64/libPhysX3* " .. CROWN_BUILD_DIR .. "linux64/bin",
+			"cp -r " .. "$(PHYSX_SDK_LINUX)/Bin/linux64/libPhysX3* " .. CROWN_ENGINE_DIR .. "linux64/bin",
 		}
 	end
 
 configuration { "x32", "vs*" }
 	postbuildcommands {
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x86/luajit.exe " .. CROWN_BUILD_DIR .. "win32/bin",
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x86/lua51.lib "  .. CROWN_BUILD_DIR .. "win32/bin",
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x86/lua51.exp "  .. CROWN_BUILD_DIR .. "win32/bin",
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x86/lua51.dll "  .. CROWN_BUILD_DIR .. "win32/bin",
-		"cp -r " .. CROWN_THIRD_DIR .. "luajit/src/jit "                .. CROWN_BUILD_DIR .. "win32/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x86/luajit.exe " .. CROWN_ENGINE_DIR .. "win32/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x86/lua51.lib "  .. CROWN_ENGINE_DIR .. "win32/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x86/lua51.exp "  .. CROWN_ENGINE_DIR .. "win32/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x86/lua51.dll "  .. CROWN_ENGINE_DIR .. "win32/bin",
+		"cp -r " .. CROWN_THIRD_DIR .. "luajit/src/jit "                .. CROWN_ENGINE_DIR .. "win32/bin",
 	}
 
 	if _OPTIONS["with-physx"] then
 		postbuildcommands {
-			"cp    " .. "$(PHYSX_SDK_WINDOWS)/bin/win32/PhysX3* "            .. CROWN_BUILD_DIR .. "win32/bin",
-			"cp    " .. "$(PHYSX_SDK_WINDOWS)/bin/win32/nvToolsExt32_1.dll " .. CROWN_BUILD_DIR .. "win32/bin",
+			"cp    " .. "$(PHYSX_SDK_WINDOWS)/bin/win32/PhysX3* "            .. CROWN_ENGINE_DIR .. "win32/bin",
+			"cp    " .. "$(PHYSX_SDK_WINDOWS)/bin/win32/nvToolsExt32_1.dll " .. CROWN_ENGINE_DIR .. "win32/bin",
 		}
 	end
 
 configuration { "x64", "vs*" }
 	postbuildcommands {
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x64/luajit.exe " .. CROWN_BUILD_DIR .. "win64/bin",
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x64/lua51.lib "  .. CROWN_BUILD_DIR .. "win64/bin",
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x64/lua51.exp "  .. CROWN_BUILD_DIR .. "win64/bin",
-		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x64/lua51.dll "  .. CROWN_BUILD_DIR .. "win64/bin",
-		"cp -r " .. CROWN_THIRD_DIR .. "luajit/src/jit "                .. CROWN_BUILD_DIR .. "win64/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x64/luajit.exe " .. CROWN_ENGINE_DIR .. "win64/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x64/lua51.lib "  .. CROWN_ENGINE_DIR .. "win64/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x64/lua51.exp "  .. CROWN_ENGINE_DIR .. "win64/bin",
+		"cp    " .. CROWN_THIRD_DIR .. "luajit/pre/win_x64/lua51.dll "  .. CROWN_ENGINE_DIR .. "win64/bin",
+		"cp -r " .. CROWN_THIRD_DIR .. "luajit/src/jit "                .. CROWN_ENGINE_DIR .. "win64/bin",
 	}
 
 	if _OPTIONS["with-physx"] then
 		postbuildcommands {
-			"cp    " .. "$(PHYSX_SDK_WINDOWS)/bin/win64/PhysX3* "            .. CROWN_BUILD_DIR .. "win64/bin",
-			"cp    " .. "$(PHYSX_SDK_WINDOWS)/bin/win64/nvToolsExt64_1.dll " .. CROWN_BUILD_DIR .. "win64/bin",
+			"cp    " .. "$(PHYSX_SDK_WINDOWS)/bin/win64/PhysX3* "            .. CROWN_ENGINE_DIR .. "win64/bin",
+			"cp    " .. "$(PHYSX_SDK_WINDOWS)/bin/win64/nvToolsExt64_1.dll " .. CROWN_ENGINE_DIR .. "win64/bin",
 		}
 	end
--- makefile
+++ makefile
@@ -15,43 +15,43 @@ GENIE=third/bx/tools/bin/$(OS)/genie
 android-build:
 	$(GENIE) --file=genie/genie.lua --with-luajit --with-openal --with-bullet --compiler=android-arm gmake
 android-arm-debug: android-build
-	make -R -C build/projects/android config=debug
+	make -R -C build/engine/projects/android config=debug
 android-arm-development: android-build
-	make -R -C build/projects/android config=development
+	make -R -C build/engine/projects/android config=development
 android-arm-release: android-build
-	make -R -C build/projects/android config=release
+	make -R -C build/engine/projects/android config=release
 android-arm: android-arm-debug android-arm-development android-arm-release
 
 linux-build:
 	$(GENIE) --file=genie/genie.lua --with-luajit --with-openal --with-bullet --with-tools --compiler=linux-gcc gmake
 linux-debug32: linux-build
-	make -R -C build/projects/linux config=debug32
+	make -R -C build/engine/projects/linux config=debug32
 linux-development32: linux-build
-	make -R -C build/projects/linux config=development32
+	make -R -C build/engine/projects/linux config=development32
 linux-release32: linux-build
-	make -R -C build/projects/linux config=release32
+	make -R -C build/engine/projects/linux config=release32
 linux-debug64: linux-build
-	make -R -C build/projects/linux config=debug64
+	make -R -C build/engine/projects/linux config=debug64
 linux-development64: linux-build
-	make -R -C build/projects/linux config=development64
+	make -R -C build/engine/projects/linux config=development64
 linux-release64: linux-build
-	make -R -C build/projects/linux config=release64
+	make -R -C build/engine/projects/linux config=release64
 linux: linux-debug32 linux-development32 linux-release32 linux-debug64 linux-development64 linux-release64
 
 windows-build:
 	$(GENIE) --file=genie\genie.lua --with-luajit --with-openal --with-bullet --with-tools vs2013
 windows-debug32: windows-build
-	devenv build/projects/vs2013/crown.sln /Build "debug|Win32"
+	devenv build/engine/projects/vs2013/crown.sln /Build "debug|Win32"
 windows-development32: windows-build
-	devenv build/projects/vs2013/crown.sln /Build "development|Win32"
+	devenv build/engine/projects/vs2013/crown.sln /Build "development|Win32"
 windows-release32: windows-build
-	devenv build/projects/vs2013/crown.sln /Build "release|Win32"
+	devenv build/engine/projects/vs2013/crown.sln /Build "release|Win32"
 windows-debug64: windows-build
-	devenv build/projects/vs2013/crown.sln /Build "debug|x64"
+	devenv build/engine/projects/vs2013/crown.sln /Build "debug|x64"
 windows-development64: windows-build
-	devenv build/projects/vs2013/crown.sln /Build "development|x64"
+	devenv build/engine/projects/vs2013/crown.sln /Build "development|x64"
 windows-release64: windows-build
-	devenv build/projects/vs2013/crown.sln /Build "release|x64"
+	devenv build/engine/projects/vs2013/crown.sln /Build "release|x64"
 
 tools-build:
 	$(GENIE) --file=genie/tools.lua gmake
