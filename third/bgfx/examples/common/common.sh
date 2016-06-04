fe355a8c899fc377d443fc112abc7232f0c89a8c
Daniele Bartolini <dbartolini.aa@gmail.com>
2016-06-04 15:03:53 +0200

    Delete BGFX examples folder

--- third/bgfx/examples/common/common.sh
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright 2011-2016 Branimir Karadzic. All rights reserved.
- * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
- */
-
-#include <bgfx_shader.sh>
-#include "shaderlib.sh"
