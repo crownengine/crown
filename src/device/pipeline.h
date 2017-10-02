e28b181a5c32c08c17c2862a7c4a98ca5776253c
Daniele Bartolini <dbartolini.aa@gmail.com>
2017-10-02 21:12:08 +0200

    Fix build

---
+++ src/device/pipeline.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2012-2017 Daniele Bartolini and individual contributors.
+ * License: https://github.com/dbartolini/crown/blob/master/LICENSE
+ */
+
+#pragma once
+
+#define VIEW_SPRITE_0   0
+#define VIEW_SPRITE_1   1
+#define VIEW_SPRITE_2   2
+#define VIEW_SPRITE_3   3
+#define VIEW_SPRITE_4   4
+#define VIEW_SPRITE_5   5
+#define VIEW_SPRITE_6   6
+#define VIEW_SPRITE_7   7
+#define VIEW_MESH      16
+#define VIEW_DEBUG     17
+#define VIEW_GUI      128
