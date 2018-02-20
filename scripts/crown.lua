d2e9075edc7def049f4a2f8b6dd3ff8613cb300f
Daniele Bartolini <dbartolini.aa@gmail.com>
2018-02-21 00:02:54 +0100

    Fix win32 build

--- scripts/crown.lua
+++ scripts/crown.lua
@@ -41,7 +41,7 @@ function crown_project(_name, _kind, _defines)
 				}
 			configuration { "x32", "vs*" }
 				libdirs {
-					CROWN_DIR .. "3rdparty/luajit/pre/win_x86"
+					CROWN_DIR .. "3rdparty/luajit/pre/win_x32"
 				}
 			configuration { "x64", "vs*" }
 				libdirs {
