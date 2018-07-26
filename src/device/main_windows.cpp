b707be4de924ddcb5a1177f0de0ed5214d20c53d
Daniele Bartolini <dbartolini.aa@gmail.com>
2018-07-26 22:10:47 +0200

    device: cleanup

--- src/device/main_windows.cpp
+++ src/device/main_windows.cpp
@@ -18,7 +18,7 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <windowsx.h>
+#include <windowsx.h> // GET_X_LPARAM, GET_Y_LPARAM
 #include <xinput.h>
 
 namespace crown
