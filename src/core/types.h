05b693ca35f8e5c38cc50f6e86a74afa9a6d0c28
Daniele Bartolini <dbartolini.aa@gmail.com>
2019-05-04 17:28:43 +0200

    core: add CE_STRINGIZE()

--- src/core/types.h
+++ src/core/types.h
@@ -78,6 +78,9 @@ inline T clamp(T val, T mmin, T mmax)
 #define countof(arr) (sizeof(arr)/sizeof(arr[0]))
 #define container_of(ptr, type, member) ((char*)ptr - offsetof(type, member))
 
+#define CE_STRINGIZE(value) CE_STRINGIZE_(value)
+#define CE_STRINGIZE_(value) #value
+
 #define CE_NOOP(...) do { (void)0; } while (0)
 #define CE_UNUSED(x) do { (void)(x); } while (0)
 #define CE_STATIC_ASSERT(condition, ...) static_assert(condition, "" # __VA_ARGS__)
