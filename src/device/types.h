93a4ddcbe685a3bc5e300bd448f46bbacb61d901
Daniele Bartolini <dbartolini.aa@gmail.com>
2019-05-15 23:17:19 +0200

    device: add device/types.h

---
+++ src/device/types.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2012-2018 Daniele Bartolini and individual contributors.
+ * License: https://github.com/dbartolini/crown/blob/master/LICENSE
+ */
+
+#pragma once
+
+namespace crown
+{
+struct ConsoleServer;
+struct Device;
+struct Pipeline;
+struct Profiler;
+
+} // namespace crown
