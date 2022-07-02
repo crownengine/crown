aad5dbae2a1a220a56ab4afd4ace15a8d53d3a75
Daniele Bartolini <dbartolini.aa@gmail.com>
2022-07-03 00:26:36 +0200

    scripts: patterns must be quoted

--- scripts/uncrustify/format-all.sh
+++ scripts/uncrustify/format-all.sh
@@ -1,9 +1,9 @@
-find src/ -iname *.h -o -iname *.inl -o -iname *.cpp \
-	| tr '\n' '\0'                                   \
-	| xargs -0 -n1 bash -c                           \
+find src/ -iname '*.h' -o -iname '*.inl' -o -iname '*.cpp' \
+	| tr '\n' '\0'                                         \
+	| xargs -0 -n1 bash -c                                 \
 		'cat $0 | ./scripts/uncrustify/uncrustify-wrapper.sh scripts/uncrustify/cpp.cfg > $0.new && mv $0.new $0'
 
-find tools/ -iname *.vala -o -iname *.c \
-	| tr '\n' '\0'                      \
-	| xargs -0 -n1 bash -c \
+find tools/ -iname '*.vala' -o -iname '*.c' \
+	| tr '\n' '\0'                          \
+	| xargs -0 -n1 bash -c                  \
 		'cat $0 | ./scripts/uncrustify/uncrustify-wrapper.sh scripts/uncrustify/vala.cfg > $0.new && mv $0.new $0'
