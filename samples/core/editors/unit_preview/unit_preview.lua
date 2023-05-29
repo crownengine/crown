428e83726dcbee6afda9375a89a4a28c1b177454
Daniele Bartolini <dbartolini@crownengine.org>
2023-05-29 22:15:17 +0200

    samples: use UnitBox in UnitPreview
    Part-of: #153

--- samples/core/editors/unit_preview/unit_preview.lua
+++ samples/core/editors/unit_preview/unit_preview.lua
@@ -2,6 +2,7 @@
 -- SPDX-License-Identifier: MIT
 
 require "core/editors/level_editor/camera"
+require "core/editors/unit"
 
 UnitPreview = UnitPreview or {}
 
@@ -11,8 +12,7 @@ function UnitPreview:init()
 	self._rw = World.render_world(self._world)
 	self._pw = World.physics_world(self._world)
 	self._camera = Camera(self._world, World.spawn_unit(self._world, "core/units/camera"))
-	self._unit_name = nil
-	self._unit_id = nil
+	self._object = nil
 
 	World.spawn_unit(self._world, "core/units/light", Vector3(1000, 1000, -1000))
 end
@@ -20,21 +20,18 @@ end
 function UnitPreview:update(dt)
 	World.update(self._world, dt)
 
-	if self._unit_id then
-		local mesh = RenderWorld.mesh_instance(self._rw, self._unit_id)
-		local tm, hext = mesh
-			and RenderWorld.mesh_obb(self._rw, mesh)
-			or Matrix4x4.identity(), Vector3(1, 1, 1)
+	if self._object then
+		local obb_tm, obb_he = self._object:obb()
 
-		local radius = Vector3.length(hext)
+		local radius = Vector3.length(obb_he)
 		radius = math.ceil(radius / 2)  * 2
 		radius = radius <   1 and   1 or radius
 		radius = radius > 100 and 100 or radius
 
 		local camera_unit = self._camera:unit()
 		local pos = Vector3(radius, radius, -radius) * 2
-		local camera_pos = Matrix4x4.translation(tm) + pos
-		local target_pos = Matrix4x4.translation(tm)
+		local camera_pos = Matrix4x4.translation(obb_tm) + pos
+		local target_pos = Matrix4x4.translation(obb_tm)
 		local tr = SceneGraph.instance(self._sg, camera_unit)
 		SceneGraph.set_local_rotation(self._sg, tr, Quaternion.look(Vector3.normalize(target_pos - camera_pos)))
 		SceneGraph.set_local_position(self._sg, tr, camera_pos)
@@ -52,28 +49,20 @@ function UnitPreview:shutdown()
 end
 
 function UnitPreview:set_preview_resource(type, name)
-	local unit = nil
-	if type == "unit" then
-		unit = name
-	elseif type == "sound" then
-		unit = "core/units/sound"
-	else
+	if type == "sound" then
+		name = "core/units/sound"
+	elseif type ~= "unit" then
 		return
 	end
 
-	if self._unit_name == unit then
-		return
+	if self._object ~= nil then
+		if self._object:prefab() == name then return end
+		self._object:destroy()
 	end
 
-	if self._unit_id then
-		World.destroy_unit(self._world, self._unit_id)
-	end
-
-	self._unit_name = unit
-	self._unit_id = World.spawn_unit(self._world, unit)
-
-	local actor = PhysicsWorld.actor_instance(self._pw, self._unit_id)
-	if actor then
-		PhysicsWorld.actor_set_kinematic(self._pw, actor, true)
-	end
+	self._object = UnitBox(self._world
+		, Device.guid()
+		, World.spawn_unit(self._world, name)
+		, name
+		)
 end
