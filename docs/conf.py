c75faedf524f6c8f3d345e8f6e9bb7116373cbb5
Daniele Bartolini <dbartolini@crownengine.org>
2024-11-14 22:13:41 +0000

    docs: customize some colors

--- docs/conf.py
+++ docs/conf.py
@@ -101,8 +101,9 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 html_theme_path = ["_themes"]
+html_css_files = ["css/custom.css"]
 
 
 # -- Options for HTMLHelp output ------------------------------------------
