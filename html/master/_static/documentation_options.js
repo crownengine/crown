cc148e6d26d54c4c54ac6a1ba56426fde77a534f
Daniele Bartolini <dbartolini@crownengine.org>
2024-11-14 22:20:47 +0000

    Docs master

--- html/master/_static/documentation_options.js
+++ html/master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.51.0',
+    VERSION: '0.53.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
