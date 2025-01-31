c30cfa7373b352edf9ab4f919aa15b6fee2f0cce
Daniele Bartolini <dbartolini@crownengine.org>
2025-01-31 23:19:23 +0100

    *: require vs2022
    Also switch to windows-2022 GH Actions runner.

--- scripts/dist/windows-release.bat
+++ scripts/dist/windows-release.bat
@@ -1,3 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 make tools-windows-release64
 make windows-release64
