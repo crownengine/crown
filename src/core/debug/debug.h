8b9d33ed5fe9e6b14f83b003a6ed27772029d035
Daniele Bartolini <dbartolini@crownengine.org>
2025-04-20 00:09:07 +0200

    core: add debug::breakpoint()

---
+++ src/core/debug/debug.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2012-2025 Daniele Bartolini et al.
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include "core/types.h"
+
+/// @defgroup Debug Debug
+/// @ingroup Core
+namespace crown
+{
+/// Debug utils.
+///
+/// @ingroup Debug
+namespace debug
+{
+	///
+	void breakpoint();
+
+} // namespace debug
+
+} // namespace crown
