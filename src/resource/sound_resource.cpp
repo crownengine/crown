caf03e7b31f298fa6fc952866eb96ec4f238d6e0
Daniele Bartolini <dbartolini@crownengine.org>
2025-05-06 01:50:31 +0200

    resource: move WAV parsing to its own file
    Also, add support to 8-bit PCM data.

--- src/resource/sound_resource.cpp
+++ src/resource/sound_resource.cpp
@@ -12,6 +12,7 @@
 #include "core/memory/temp_allocator.inl"
 #include "core/strings/dynamic_string.inl"
 #include "resource/compile_options.inl"
+#include "resource/sound.h"
 #include "resource/sound_resource.h"
 
 namespace crown
@@ -28,56 +29,12 @@ namespace sound_resource
 #if CROWN_CAN_COMPILE
 namespace sound_resource_internal
 {
-	struct WAVHeader
-	{
-		char riff[4];        // Should contain 'RIFF'
-		s32 chunk_size;      // Not Needed
-		char wave[4];        // Should contain 'WAVE'
-		char fmt[4];         // Should contain 'fmt '
-		s32 fmt_size;        // Size of format chunk
-		s16 fmt_tag;         // Identifies way data is stored, 1 means no compression
-		s16 fmt_channels;    // Channel, 1 means mono, 2 means stereo
-		s32 fmt_sample_rate; // Samples per second
-		s32 fmt_avarage;     // Average bytes per sample
-		s16 fmt_block_align; // Block alignment
-		s16 fmt_bits_ps;     // Number of bits per sample
-		char data[4];        // Should contain 'data'
-		s32 data_size;       // Data dimension
-	};
-
 	s32 compile(CompileOptions &opts)
 	{
-		Buffer buf = opts.read();
-
-		TempAllocator4096 ta;
-		JsonObject obj(ta);
-		RETURN_IF_ERROR(sjson::parse(obj, buf), opts);
-
-		DynamicString name(ta);
-		RETURN_IF_ERROR(sjson::parse_string(name, obj["source"]), opts);
-
-		Buffer sound = opts.read(name.c_str());
-		const WAVHeader *wav = (const WAVHeader *)array::begin(sound);
-
-		// Write.
-		SoundResource sr;
-		sr.version     = RESOURCE_HEADER(RESOURCE_VERSION_SOUND);
-		sr.format      = SoundFormat::PCM;
-		sr.size        = wav->data_size;
-		sr.sample_rate = wav->fmt_sample_rate;
-		sr.channels    = wav->fmt_channels;
-		sr.bit_depth   = wav->fmt_bits_ps;
-
-		opts.write(sr.version);
-		opts.write(sr.format);
-		opts.write(sr.size);
-		opts.write(sr.sample_rate);
-		opts.write(sr.channels);
-		opts.write(sr.bit_depth);
-
-		opts.write(&wav[1], sr.size);
-
-		return 0;
+		Sound s(default_allocator());
+		s32 err = sound::parse(s, opts);
+		ENSURE_OR_RETURN(err == 0, opts);
+		return sound::write(s, opts);
 	}
 
 } // namespace sound_resource_internal
