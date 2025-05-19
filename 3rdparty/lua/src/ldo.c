ea45654a4f52041ccbdfd38c89e3b3ba115ee3a0
npt-1707 <npthanh132@gmail.com>
2025-05-20 04:21:11 +0800

    disable loading lua bytecode

--- 3rdparty/lua/src/ldo.c
+++ 3rdparty/lua/src/ldo.c
@@ -495,8 +495,7 @@ static void f_parser (lua_State *L, void *ud) {
   struct SParser *p = cast(struct SParser *, ud);
   int c = luaZ_lookahead(p->z);
   luaC_checkGC(L);
-  tf = ((c == LUA_SIGNATURE[0]) ? luaU_undump : luaY_parser)(L, p->z,
-                                                             &p->buff, p->name);
+  tf = (luaY_parser)(L, p->z, &p->buff, p->name);
   cl = luaF_newLclosure(L, tf->nups, hvalue(gt(L)));
   cl->l.p = tf;
   for (i = 0; i < tf->nups; i++)  /* initialize eventual upvalues */
