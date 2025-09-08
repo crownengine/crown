e4d7630b07b3ee17d1ece1d9a9cc607cd2ff84ac
Daniele Bartolini <dbartolini@crownengine.org>
2025-09-09 01:19:05 +0200

    device: add MSAA support

--- src/resource/render_config_resource.h
+++ src/resource/render_config_resource.h
@@ -18,7 +18,8 @@ struct RenderSettingsFlags
 		LOCAL_LIGHTS                  = u32(1) << 1, ///< Whether local lights are enabled.
 		LOCAL_LIGHTS_SHADOWS          = u32(1) << 2, ///< Whether shadows for local lights are enabled.
 		LOCAL_LIGHTS_DISTANCE_CULLING = u32(1) << 3, ///< Whether distance culling for local lights is enabled.
-		BLOOM                         = u32(1) << 4  ///< Whether bloom post-processing effect is enabled.
+		BLOOM                         = u32(1) << 4, ///< Whether bloom post-processing effect is enabled.
+		MSAA                          = u32(1) << 5  ///< Whether multisample AA is enabled.
 	};
 };
 
@@ -29,6 +30,7 @@ struct RenderSettings
 	Vector2 local_lights_shadow_map_size;
 	f32 local_lights_distance_culling_fade;   ///< Distance from camera at which local lights start to fade.
 	f32 local_lights_distance_culling_cutoff; ///< Distance from camera at which local lights disappear.
+	u32 msaa_quality;
 };
 
 struct RenderConfigResource
