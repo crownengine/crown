3793186aaf276d901cec18635b569f49e6726904
Daniele Bartolini <dbartolini@crownengine.org>
2025-10-12 23:47:21 +0200

    scripts: dist: cleanup

--- scripts/dist/bump.sh
+++ scripts/dist/bump.sh
@@ -6,6 +6,29 @@ set -eu
 
 . scripts/dist/version.sh
 
+crown_docs_append_changelog_version () {
+	VERSION_STRING=$(crown_version_string ${1} ${2} ${3})
+	# Prepare CHANGELOG for next version.
+	# Create a stable target to the heading based on version number.
+	# Use the heading title to reference the very same target to avoid Sphinx discarding it.
+	CHANGELOG_HEAD_TARGET=".. _v${VERSION_STRING}:"
+	CHANGELOG_HEAD=":ref:\`${VERSION_STRING} --- DD MMM YYYY <v${VERSION_STRING}>\`"
+	CHANGELOG_UNDERLINE=$(echo "${CHANGELOG_HEAD}" | sed 's/./-/g') # Replace all chars in CHANGELOG_HEAD with '-'
+
+	{
+		echo "Changelog"
+		echo "========="
+		echo ""
+		echo "${CHANGELOG_HEAD_TARGET}"
+		echo ""
+		echo "${CHANGELOG_HEAD}"
+		echo "${CHANGELOG_UNDERLINE}"
+	} >> docs/changelog.rst.next
+
+	tail docs/changelog.rst -n +3 >> docs/changelog.rst.next
+	mv docs/changelog.rst.next docs/changelog.rst
+}
+
 if [ $# -ne 3 ]; then
 	echo "Specify next version number."
 	echo ""
@@ -15,46 +38,15 @@ fi
 
 git checkout master
 
-# Read current version
-VERSION_MAJOR=$(crown_version_major)
-VERSION_MINOR=$(crown_version_minor)
-VERSION_PATCH=$(crown_version_patch)
-VERSION=$(crown_version)
-
 # The new version to be bumped
 VERSION_NEXT_MAJOR=$1
 VERSION_NEXT_MINOR=$2
 VERSION_NEXT_PATCH=$3
-VERSION_NEXT="${VERSION_NEXT_MAJOR}.${VERSION_NEXT_MINOR}.${VERSION_NEXT_PATCH}"
+VERSION_NEXT=$(crown_version_string ${VERSION_NEXT_MAJOR} ${VERSION_NEXT_MINOR} ${VERSION_NEXT_PATCH})
 
-# Patch version numbers in config.h
-sed -i "s/MAJOR ${VERSION_MAJOR}/MAJOR ${VERSION_NEXT_MAJOR}/g" src/config.h
-sed -i "s/MINOR ${VERSION_MINOR}/MINOR ${VERSION_NEXT_MINOR}/g" src/config.h
-sed -i "s/PATCH ${VERSION_PATCH}/PATCH ${VERSION_NEXT_PATCH}/g" src/config.h
-# Patch version string in config.vala
-sed -i "s/${VERSION}/${VERSION_NEXT}/g" tools/config.vala
-# Patch version string in docs
-sed -i "s/${VERSION}/${VERSION_NEXT}/g" docs/conf.py
-
-# Prepare CHANGELOG for next version.
-# Create a stable target to the heading based on version number.
-# Use the heading title to reference the very same target to avoid Sphinx discarding it.
-CHANGELOG_HEAD_TARGET=".. _v${VERSION_NEXT}:"
-CHANGELOG_HEAD=":ref:\`${VERSION_NEXT} --- DD MMM YYYY <v${VERSION_NEXT}>\`"
-CHANGELOG_UNDERLINE=$(echo "${CHANGELOG_HEAD}" | sed 's/./-/g') # Replace all chars in CHANGELOG_HEAD with '-'
-
-{
-	echo "Changelog"
-	echo "========="
-	echo ""
-	echo "${CHANGELOG_HEAD_TARGET}"
-	echo ""
-	echo "${CHANGELOG_HEAD}"
-	echo "${CHANGELOG_UNDERLINE}"
-} >> docs/changelog.rst.next
-
-tail docs/changelog.rst -n +3 >> docs/changelog.rst.next
-mv docs/changelog.rst.next docs/changelog.rst
+crown_set_version $VERSION_NEXT_MAJOR $VERSION_NEXT_MINOR $VERSION_NEXT_PATCH
+crown_docs_set_version $VERSION_NEXT_MAJOR $VERSION_NEXT_MINOR $VERSION_NEXT_PATCH
+crown_docs_append_changelog_version $VERSION_NEXT_MAJOR $VERSION_NEXT_MINOR $VERSION_NEXT_PATCH
 
 echo "Crown v${VERSION_NEXT} will be bumped"
 echo "Continue? [y/N]"
