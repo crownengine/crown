b2becdd52f2f7d0eae9da7b48342de67a7dec3bc
Daniele Bartolini <dbartolini@crownengine.org>
2025-10-14 01:05:24 +0200

    core: add end-of-options marker

--- src/core/command_line.cpp
+++ src/core/command_line.cpp
@@ -27,9 +27,20 @@ static bool is_longopt(const char *arg, const char *longopt)
 		;
 }
 
+static bool is_end_options_marker(const char *arg)
+{
+	return strlen32(arg) == 2
+		&& arg[0] == '-'
+		&& arg[1] == '-'
+		;
+}
+
 static int find_option(int argc, const char **argv, const char *longopt, char shortopt)
 {
 	for (int i = 0; i < argc; ++i) {
+		if (is_end_options_marker(argv[i]))
+			break;
+
 		if (is_longopt(argv[i], longopt) || is_shortopt(argv[i], shortopt))
 			return i;
 	}
@@ -45,8 +56,18 @@ CommandLine::CommandLine(int argc, const char **argv)
 
 const char *CommandLine::get_parameter(int i, const char *longopt, char shortopt)
 {
-	int argc = find_option(_argc, _argv, longopt, shortopt);
-	return argc + i < _argc ? _argv[argc + i + 1] : NULL;
+	const int opt_in = find_option(_argc, _argv, longopt, shortopt);
+	const int arg_in = opt_in + 1 + i;
+
+	if (arg_in >= _argc)
+		return NULL;
+
+	for (int j = opt_in; j < arg_in; ++j) {
+		if (is_end_options_marker(_argv[j + 1]))
+			return NULL;
+	}
+
+	return _argv[arg_in];
 }
 
 bool CommandLine::has_option(const char *longopt, char shortopt)
