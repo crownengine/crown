5828ed24b84912b9f449f5d2966dcbfb3c5a713d
Daniele Bartolini <dbartolini@crownengine.org>
2025-10-16 10:33:11 +0200

    docs: use monokai pygments theme

--- docs/conf.py
+++ docs/conf.py
@@ -71,7 +71,7 @@ language = 'en'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'monokai'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
