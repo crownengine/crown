4f7620ab674eff51f81a13f3adcef9acc3ce7476
Daniele Bartolini <dbartolini@crownengine.org>
2026-01-29 00:46:24 +0100

    device: cleanup

--- src/device/pipeline.h
+++ src/device/pipeline.h
@@ -126,9 +126,6 @@ struct Pipeline
 	bgfx::UniformHandle _html5_default_sampler;
 	bgfx::TextureHandle _html5_default_texture;
 
-	bgfx::UniformHandle _u_uv_scale;
-	bgfx::UniformHandle _u_uv_offset;
-
 	// Default shaders.
 	ShaderData _blit_shader;
 	ShaderData _blit_blend_shader;
