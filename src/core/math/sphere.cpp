a43d89cf2b70aed0af6ea2d3c81fd5af30d6308f
Daniele Bartolini <dbartolini@crownengine.org>
2026-02-25 17:55:27 +0100

    core: fix Sphere::add_points()
    Also drop unused add_spheres().

--- src/core/math/sphere.cpp
+++ src/core/math/sphere.cpp
@@ -16,29 +16,29 @@ namespace sphere
 		s.r = 0.0f;
 	}
 
+	// Reference:
+	// https://web.archive.org/web/20170808221957/https://sharif.edu/~zarrabi/papers/cccg-06/meb.pdf
 	void add_points(Sphere &s, u32 num, u32 stride, const void *points)
 	{
-		float rr = s.r*s.r;
-
 		const char *pts = (char *)points;
-		for (u32 i = 0; i < num; ++i, pts += stride) {
-			const Vector3 &pi = *(Vector3 *)pts;
 
-			rr = max(rr, length_squared(pi - s.c));
-		}
+		if (CE_UNLIKELY(num == 0))
+			return;
 
-		s.r = fsqrt(rr);
-	}
+		s.c = *(Vector3 *)pts;
+		s.r = 0.0f;
+		pts += stride;
 
-	void add_spheres(Sphere &s, u32 num, const Sphere *spheres)
-	{
-		for (u32 i = 0; i < num; ++i) {
-			const Sphere &si = spheres[i];
-			const f32 dist = length_squared(si.c - s.c);
+		for (u32 i = 1; i < num; ++i, pts += stride) {
+			const Vector3 &p = *(Vector3 *)pts;
 
-			if (dist < (si.r + s.r) * (si.r + s.r)) {
-				if (si.r*si.r > s.r*s.r)
-					s.r = fsqrt(dist + si.r*si.r);
+			if (!sphere::contains_point(s, p)) {
+				const f32 dist  = distance(p, s.c);
+				const f32 delta = 0.5f * (dist - s.r);
+				const f32 alpha = delta / dist;
+
+				s.c += alpha * (p - s.c);
+				s.r += delta + FLOAT_EPSILON * 10.0f;
 			}
 		}
 	}
