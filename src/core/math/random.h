c37dbb8c38153cd48b44b7058b2a3ebf4c7300a0
Daniele Bartolini <dbartolini@crownengine.org>
2026-02-25 17:53:16 +0100

    core: move inline funcs to .inl

--- src/core/math/random.h
+++ src/core/math/random.h
@@ -29,25 +29,4 @@ struct Random
 	f32 unit_float();
 };
 
-inline Random::Random(s32 seed)
-	: _seed(seed)
-{
-}
-
-inline s32 Random::integer()
-{
-	_seed = 214013 * _seed + 13737667;
-	return (_seed >> 16) & 0x7fff;
-}
-
-inline s32 Random::integer(s32 max)
-{
-	return (max == 0) ? 0 : integer() % max;
-}
-
-inline f32 Random::unit_float()
-{
-	return integer() / (f32)0x7fff;
-}
-
 } // namespace crown
