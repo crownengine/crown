2c0c6880375265b2e1d569a086dde7c61f1d604b
Daniele Bartolini <dbartolini@crownengine.org>
2026-03-03 19:20:33 +0100

    device: add logd()

--- src/device/log.h
+++ src/device/log.h
@@ -5,6 +5,7 @@
 
 #pragma once
 
+#include "config.h"
 #include <stdarg.h>
 
 namespace crown
@@ -19,6 +20,7 @@ struct LogSeverity
 		LOG_INFO,
 		LOG_WARN,
 		LOG_ERROR,
+		LOG_DEBUG,
 
 		COUNT
 	};
@@ -41,8 +43,14 @@ namespace log_internal
 #define vlogi(system, format, va_list) crown::log_internal::vlogx(crown::LogSeverity::LOG_INFO, system, format, va_list)
 #define vloge(system, format, va_list) crown::log_internal::vlogx(crown::LogSeverity::LOG_ERROR, system, format, va_list)
 #define vlogw(system, format, va_list) crown::log_internal::vlogx(crown::LogSeverity::LOG_WARN, system, format, va_list)
+#define vlogd(system, format, va_list) crown::log_internal::vlogx(crown::LogSeverity::LOG_DEBUG, system, format, va_list)
 #define logi(system, format, ...) crown::log_internal::logx(crown::LogSeverity::LOG_INFO, system, format, ## __VA_ARGS__)
 #define loge(system, format, ...) crown::log_internal::logx(crown::LogSeverity::LOG_ERROR, system, format, ## __VA_ARGS__)
 #define logw(system, format, ...) crown::log_internal::logx(crown::LogSeverity::LOG_WARN, system, format, ## __VA_ARGS__)
+#if CROWN_DEBUG && !CROWN_DEVELOPMENT
+#   define logd(system, format, ...) crown::log_internal::logx(crown::LogSeverity::LOG_DEBUG, system, format, ## __VA_ARGS__)
+#else
+#   define logd(...) CE_NOOP()
+#endif
 
 #define LOG_SYSTEM(ident, name) namespace { const crown::log_internal::System ident = { name }; }
