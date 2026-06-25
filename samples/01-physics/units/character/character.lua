1603d147d02e834dc4e04f09e3ee84a042ac8825
Daniele Bartolini <dbartolini@crownengine.org>
2026-06-25 03:30:56 +0200

    samples: 01-physics: add footstep sounds

--- samples/01-physics/units/character/character.lua
+++ samples/01-physics/units/character/character.lua
@@ -34,4 +34,37 @@ end
 function UnitsCharacterCharacter.mover_mover_collision(_, mover_unit, other_mover_unit, mover, other_mover)
 end
 
+function UnitsCharacterCharacter.event(world, unit, event_name)
+	local footsteps = {
+		"sfx/footsteps/floor/floor_001",
+		"sfx/footsteps/floor/floor_002",
+		"sfx/footsteps/floor/floor_003",
+		"sfx/footsteps/floor/floor_004",
+		"sfx/footsteps/floor/floor_005",
+		"sfx/footsteps/floor/floor_006",
+		"sfx/footsteps/floor/floor_007",
+		"sfx/footsteps/floor/floor_008",
+		"sfx/footsteps/floor/floor_009",
+		"sfx/footsteps/floor/floor_010",
+		"sfx/footsteps/floor/floor_011",
+		"sfx/footsteps/floor/floor_012",
+		"sfx/footsteps/floor/floor_013",
+		"sfx/footsteps/floor/floor_014",
+		"sfx/footsteps/floor/floor_015",
+		"sfx/footsteps/floor/floor_016",
+		"sfx/footsteps/floor/floor_017",
+		"sfx/footsteps/floor/floor_018",
+		"sfx/footsteps/floor/floor_019",
+		"sfx/footsteps/floor/floor_020",
+		"sfx/footsteps/floor/floor_021",
+	}
+
+	if event_name == Device.string_id("footsteps_L") or event_name == Device.string_id("footsteps_R") then
+		local scene_graph = World.scene_graph(world)
+		local transform = SceneGraph.instance(scene_graph, unit)
+		local position = SceneGraph.local_position(scene_graph, transform)
+		World.play_sound(world, footsteps[math.random(21)], false, 0.1, 50, position)
+	end
+end
+
 return UnitsCharacterCharacter
