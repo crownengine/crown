84a9d84c3b5ccb1ddee2bbcebadade4bbbc372f4
Daniele Bartolini <dbartolini@crownengine.org>
2026-06-30 01:37:47 +0200

    samples: 01-physics: control character through unit script

--- samples/01-physics/main.lua
+++ samples/01-physics/main.lua
@@ -2,7 +2,6 @@
 -- SPDX-License-Identifier: MIT
 
 require "core/game/camera"
-require "character"
 
 Game = Game or {
 	physics_world = nil,
@@ -13,8 +12,6 @@ Game = Game or {
 	mover_debug_line = nil,
 	cursor_disabled = false,
 	last_input_is_pad = false,
-	character = nil,
-	pad_look_speed = 32,
 
 	-- Bullets management.
 	max_bullets = 20,
@@ -60,13 +57,6 @@ function Game.level_loaded()
 	Game.camera = FPSCamera(GameBase.world, GameBase.camera_unit)
 	Game.world_gui = World.create_world_gui(GameBase.world)
 	Game.mover_debug_line = World.create_debug_line(GameBase.world, true)
-
-	-- Create character controller.
-	Game.character = nil
-	local character_unit = World.unit_by_name(GameBase.world, "character")
-	if character_unit then
-		Game.character = Character(GameBase.world, character_unit, Game.camera)
-	end
 
 	-- Create a pool of reusable bullets.
 	for i=1, Game.max_bullets do
@@ -115,14 +105,6 @@ function Game.update(dt)
 			end
 		else
 			Game.cursor_disabled = Window.set_cursor_mode("disabled")
-		end
-	end
-
-	-- Toggle camera mode.
-	if Keyboard.released(Keyboard.button_id("minus"))
-		or Pad1.pressed(Pad1.button_id("thumb_right")) then
-		if Game.character then
-			Game.character:toggle_camera_mode()
 		end
 	end
 
@@ -208,44 +190,6 @@ function Game.update(dt)
 				Game.door_open = not Game.door_open
 			end
 		end
-	end
-
-	-- Mouse/keyboard controls.
-	local move_dx = Keyboard.button(Keyboard.button_id("d")) - Keyboard.button(Keyboard.button_id("a"))
-	local move_dy = Keyboard.button(Keyboard.button_id("w")) - Keyboard.button(Keyboard.button_id("s"))
-	local look_dx = 0
-	local look_dy = 0
-	if Game.cursor_disabled then
-		local cursor_delta = Mouse.axis(Mouse.axis_id("cursor_delta"))
-		look_dx = look_dx + cursor_delta.x
-		look_dy = look_dy + cursor_delta.y
-	end
-
-	-- Joypad controls.
-	local pad_left = Pad1.axis(Pad1.axis_id("left"))
-	local pad_right = Pad1.axis(Pad1.axis_id("right"))
-	move_dx = move_dx + pad_left.x
-	move_dy = move_dy + pad_left.y
-	look_dx = look_dx + pad_right.x * Game.pad_look_speed
-	look_dy = look_dy - pad_right.y * Game.pad_look_speed
-
-	-- Rotate camera.
-	if math.abs(look_dx) > 0.0001 or math.abs(look_dy) > 0.0001 then
-		Game.camera:rotate(dt, look_dx, look_dy)
-	end
-
-	local jump_pressed = Keyboard.pressed(Keyboard.button_id("space"))
-		or Pad1.pressed(Pad1.button_id("a"))
-	local run_pressed = Keyboard.button(Keyboard.button_id("shift_left")) ~= 0
-		or Pad1.button(Pad1.button_id("b")) ~= 0
-	local crouch_pressed = Keyboard.button(Keyboard.button_id("ctrl_left")) ~= 0
-		or Keyboard.button(Keyboard.button_id("ctrl_right")) ~= 0
-		or Pad1.button(Pad1.button_id("shoulder_left")) ~= 0
-
-	if Game.character then
-		Game.character:update(dt, move_dx, move_dy, jump_pressed, run_pressed, crouch_pressed)
-	else
-		Game.camera:move(dt, move_dx, move_dy)
 	end
 
 	local camera_world = Game.camera:world_pose()
