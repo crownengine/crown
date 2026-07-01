a6f9fa240f4693f61c1add47d4bed66fac02b84d
Daniele Bartolini <dbartolini@crownengine.org>
2026-07-01 03:49:35 +0200

    samples: 02-animation: play footstep sounds

--- samples/02-animation/units/humanoid/humanoid.lua
+++ samples/02-animation/units/humanoid/humanoid.lua
@@ -9,6 +9,29 @@ Humanoid = Humanoid or {
 }
 
 local data = Humanoid.data
+local FOOTSTEPS = {
+	"sfx/footsteps/floor/floor_001",
+	"sfx/footsteps/floor/floor_002",
+	"sfx/footsteps/floor/floor_003",
+	"sfx/footsteps/floor/floor_004",
+	"sfx/footsteps/floor/floor_005",
+	"sfx/footsteps/floor/floor_006",
+	"sfx/footsteps/floor/floor_007",
+	"sfx/footsteps/floor/floor_008",
+	"sfx/footsteps/floor/floor_009",
+	"sfx/footsteps/floor/floor_010",
+	"sfx/footsteps/floor/floor_011",
+	"sfx/footsteps/floor/floor_012",
+	"sfx/footsteps/floor/floor_013",
+	"sfx/footsteps/floor/floor_014",
+	"sfx/footsteps/floor/floor_015",
+	"sfx/footsteps/floor/floor_016",
+	"sfx/footsteps/floor/floor_017",
+	"sfx/footsteps/floor/floor_018",
+	"sfx/footsteps/floor/floor_019",
+	"sfx/footsteps/floor/floor_020",
+	"sfx/footsteps/floor/floor_021",
+}
 
 function Humanoid.spawned(world, units)
 	local world_data = data[world]
@@ -46,6 +69,28 @@ function Humanoid.unspawned(world, units)
 	for _, unit in pairs(units) do
 		world_data[unit] = nil
 	end
+end
+
+function Humanoid.event(world, unit, event_name)
+	if event_name ~= Device.string_id("footsteps_L") and event_name ~= Device.string_id("footsteps_R") then
+		return
+	end
+
+	if Game == nil or Game.player ~= unit then
+		return
+	end
+
+	local world_data = data[world]
+	local unit_data = world_data and world_data[unit]
+	local character = unit_data and unit_data.character
+	if character == nil or not character:is_moving() then
+		return
+	end
+
+	local scene_graph = World.scene_graph(world)
+	local transform = SceneGraph.instance(scene_graph, unit)
+	local position = SceneGraph.local_position(scene_graph, transform)
+	World.play_sound(world, FOOTSTEPS[math.random(21)], false, 0.1, 50, position)
 end
 
 return Humanoid
