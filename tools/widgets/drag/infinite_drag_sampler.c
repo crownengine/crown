103011bc8e888c081e2e6959fbca6b5b2195e2cf
Daniele Bartolini <dbartolini@crownengine.org>
2026-08-27 21:55:40 +0200

    tools: cleanup

--- tools/widgets/drag/infinite_drag_sampler.c
+++ tools/widgets/drag/infinite_drag_sampler.c
@@ -7,7 +7,10 @@
 #include <gdk/gdk.h>
 #include <stdint.h>
 
-#if defined(__linux__)
+#if defined(_WIN32)
+#   define WIN32_LEAN_AND_MEAN
+#   include <windows.h>
+#elif defined(__linux__)
 #   include <errno.h>
 #   include <gdk/gdkx.h>
 #   include <poll.h>
@@ -17,10 +20,7 @@
 #   include <X11/Xlib.h>
 #   include <X11/extensions/XInput2.h>
 #   include <X11/extensions/Xfixes.h>
-#elif defined(_WIN32)
-#   define WIN32_LEAN_AND_MEAN
-#   include <windows.h>
-#endif /* if defined(__linux__) */
+#endif /* if defined(_WIN32) */
 
 #if defined(__linux__)
 static void initialize_xlib_threads(void) __attribute__((constructor));
@@ -49,16 +49,16 @@ typedef struct CrownInfiniteDragSampler
 	gint samples;
 	gint released;
 	gint cancel_requested;
-#if defined(__linux__)
-	Window x11_window;
-	gint x11_wake_fd;
-#elif defined(_WIN32)
+#if defined(_WIN32)
 	HANDLE windows_stop_event;
 	gint windows_absolute_x;
 	gint windows_absolute_y;
 	gboolean windows_absolute_initialized;
 	gboolean windows_cursor_hidden;
-#endif /* if defined(__linux__) */
+#elif defined(__linux__)
+	Window x11_window;
+	gint x11_wake_fd;
+#endif /* if defined(_WIN32) */
 } CrownInfiniteDragSampler;
 
 static void store_delta(CrownInfiniteDragSampler *sampler, gdouble delta_x, gdouble delta_y)
@@ -84,7 +84,228 @@ static void store_wheel(CrownInfiniteDragSampler *sampler, gdouble wheel_dx, gdo
 	g_mutex_unlock(&sampler->mutex);
 }
 
-#if defined(__linux__)
+#if defined(_WIN32)
+static const wchar_t WINDOWS_RAW_INPUT_CLASS_NAME[] = L"CrownInfiniteDragRawInput";
+
+/* Event-driven only: do not replace with cursor-position polling or repeated SetCursorPos calls. */
+
+static gboolean register_windows_raw_input(HWND target, gboolean preserve_legacy_events)
+{
+	RAWINPUTDEVICE mouse =
+	{
+		.usUsagePage = 0x01,
+		.usUsage = 0x02,
+		.dwFlags = target != NULL
+			? RIDEV_INPUTSINK | (preserve_legacy_events ? 0 : RIDEV_NOLEGACY)
+			: RIDEV_REMOVE,
+		.hwndTarget = target,
+	};
+	if (RegisterRawInputDevices(&mouse, 1, sizeof(mouse)))
+		return TRUE;
+
+	return FALSE;
+}
+
+static void process_windows_raw_mouse(CrownInfiniteDragSampler *sampler, const RAWMOUSE *mouse)
+{
+	gint delta_x = 0;
+	gint delta_y = 0;
+	if ((mouse->usFlags & MOUSE_MOVE_ABSOLUTE) != 0) {
+		gboolean virtual_desktop = (mouse->usFlags & MOUSE_VIRTUAL_DESKTOP) != 0;
+		gint screen_x = virtual_desktop ? GetSystemMetrics(SM_XVIRTUALSCREEN) : 0;
+		gint screen_y = virtual_desktop ? GetSystemMetrics(SM_YVIRTUALSCREEN) : 0;
+		gint screen_width = virtual_desktop ? GetSystemMetrics(SM_CXVIRTUALSCREEN) : GetSystemMetrics(SM_CXSCREEN);
+		gint screen_height = virtual_desktop ? GetSystemMetrics(SM_CYVIRTUALSCREEN) : GetSystemMetrics(SM_CYSCREEN);
+		gint absolute_x = screen_x + MulDiv(mouse->lLastX, MAX(screen_width - 1, 1), 65535);
+		gint absolute_y = screen_y + MulDiv(mouse->lLastY, MAX(screen_height - 1, 1), 65535);
+		if (sampler->windows_absolute_initialized) {
+			delta_x = absolute_x - sampler->windows_absolute_x;
+			delta_y = absolute_y - sampler->windows_absolute_y;
+		}
+		sampler->windows_absolute_x = absolute_x;
+		sampler->windows_absolute_y = absolute_y;
+		sampler->windows_absolute_initialized = TRUE;
+	} else {
+		delta_x = mouse->lLastX;
+		delta_y = mouse->lLastY;
+		sampler->windows_absolute_initialized = FALSE;
+	}
+
+	if ((mouse->usButtonFlags & RI_MOUSE_WHEEL) != 0) {
+		SHORT wheel_delta = (SHORT)mouse->usButtonData;
+		store_wheel(sampler, 0.0, -(gdouble)wheel_delta/120.0);
+	}
+	if ((mouse->usButtonFlags & RI_MOUSE_HWHEEL) != 0) {
+		SHORT wheel_delta = (SHORT)mouse->usButtonData;
+		store_wheel(sampler, (gdouble)wheel_delta/120.0, 0.0);
+	}
+
+	if (delta_x != 0 || delta_y != 0) {
+		store_delta(sampler, delta_x, delta_y);
+		store_sample(sampler);
+	}
+
+	USHORT cancel_button_down = 0;
+	switch (sampler->cancel_button) {
+	case GDK_BUTTON_PRIMARY:
+		cancel_button_down = RI_MOUSE_LEFT_BUTTON_DOWN;
+		break;
+	case GDK_BUTTON_MIDDLE:
+		cancel_button_down = RI_MOUSE_MIDDLE_BUTTON_DOWN;
+		break;
+	case GDK_BUTTON_SECONDARY:
+		cancel_button_down = RI_MOUSE_RIGHT_BUTTON_DOWN;
+		break;
+	}
+	if (cancel_button_down != 0 && (mouse->usButtonFlags & cancel_button_down) != 0) {
+		g_atomic_int_set(&sampler->cancel_requested, TRUE);
+		g_atomic_int_set(&sampler->running, FALSE);
+		SetEvent(sampler->windows_stop_event);
+		return;
+	}
+
+	USHORT trigger_button_up = 0;
+	switch (sampler->trigger_button) {
+	case GDK_BUTTON_PRIMARY:
+		trigger_button_up = RI_MOUSE_LEFT_BUTTON_UP;
+		break;
+	case GDK_BUTTON_MIDDLE:
+		trigger_button_up = RI_MOUSE_MIDDLE_BUTTON_UP;
+		break;
+	case GDK_BUTTON_SECONDARY:
+		trigger_button_up = RI_MOUSE_RIGHT_BUTTON_UP;
+		break;
+	}
+	if (trigger_button_up != 0 && (mouse->usButtonFlags & trigger_button_up) != 0) {
+		g_atomic_int_set(&sampler->released, TRUE);
+		g_atomic_int_set(&sampler->running, FALSE);
+		SetEvent(sampler->windows_stop_event);
+	}
+}
+
+static LRESULT CALLBACK windows_raw_input_window_proc(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
+{
+	if (message == WM_NCCREATE) {
+		CREATESTRUCTW *create = (CREATESTRUCTW *)lparam;
+		SetWindowLongPtrW(window, GWLP_USERDATA, (LONG_PTR)create->lpCreateParams);
+		return TRUE;
+	}
+
+	CrownInfiniteDragSampler *sampler = (CrownInfiniteDragSampler *)GetWindowLongPtrW(window, GWLP_USERDATA);
+	if (message == WM_INPUT && sampler != NULL) {
+		RAWINPUT input;
+		UINT input_size = sizeof(input);
+		UINT result = GetRawInputData((HRAWINPUT)lparam
+			, RID_INPUT
+			, &input
+			, &input_size
+			, sizeof(RAWINPUTHEADER)
+			);
+		if (result != (UINT)-1 && input.header.dwType == RIM_TYPEMOUSE) {
+			process_windows_raw_mouse(sampler, &input.data.mouse);
+		}
+	}
+
+	return DefWindowProcW(window, message, wparam, lparam);
+}
+
+static HWND create_windows_raw_input_window(CrownInfiniteDragSampler *sampler)
+{
+	HINSTANCE instance = GetModuleHandleW(NULL);
+	WNDCLASSEXW window_class =
+	{
+		.cbSize = sizeof(window_class),
+		.lpfnWndProc = windows_raw_input_window_proc,
+		.hInstance = instance,
+		.lpszClassName = WINDOWS_RAW_INPUT_CLASS_NAME,
+	};
+	if (RegisterClassExW(&window_class) == 0 && GetLastError() != ERROR_CLASS_ALREADY_EXISTS) {
+		return NULL;
+	}
+
+	HWND window = CreateWindowExW(0
+		, WINDOWS_RAW_INPUT_CLASS_NAME
+		, L""
+		, 0
+		, 0
+		, 0
+		, 0
+		, 0
+		, HWND_MESSAGE
+		, NULL
+		, instance
+		, sampler
+		);
+	return window;
+}
+
+static void drain_windows_raw_input_messages(void)
+{
+	MSG message;
+	while (PeekMessageW(&message, NULL, WM_INPUT, WM_INPUT, PM_REMOVE))
+		DispatchMessageW(&message);
+}
+
+static gpointer sample_pointer_windows(gpointer data)
+{
+	CrownInfiniteDragSampler *sampler = data;
+	HWND window = create_windows_raw_input_window(sampler);
+	gboolean registered = FALSE;
+	if (window == NULL)
+		goto setup_failed;
+
+	if (!register_windows_raw_input(window, sampler->preserve_legacy_events))
+		goto setup_failed;
+	registered = TRUE;
+
+	RECT clip =
+	{
+		.left = sampler->anchor_x,
+		.top = sampler->anchor_y,
+		.right = sampler->anchor_x + 1,
+		.bottom = sampler->anchor_y + 1,
+	};
+	if (!ClipCursor(&clip)) {
+		goto setup_failed;
+	}
+
+	while (g_atomic_int_get(&sampler->running)) {
+		DWORD wait_result = MsgWaitForMultipleObjects(1
+			, &sampler->windows_stop_event
+			, FALSE
+			, INFINITE
+			, QS_RAWINPUT
+			);
+		if (wait_result == WAIT_OBJECT_0)
+			break;
+		if (wait_result == WAIT_FAILED) {
+			break;
+		}
+
+		drain_windows_raw_input_messages();
+	}
+
+	/* GTK may observe the release just before this worker does. */
+	drain_windows_raw_input_messages();
+
+	/* Restore and unconfine before the GTK thread reveals the cursor after joining. */
+	SetCursorPos(sampler->anchor_x, sampler->anchor_y);
+	ClipCursor(NULL);
+
+	register_windows_raw_input(NULL, FALSE);
+	DestroyWindow(window);
+	return NULL;
+
+setup_failed:
+	if (registered)
+		register_windows_raw_input(NULL, FALSE);
+	if (window != NULL)
+		DestroyWindow(window);
+	g_atomic_int_set(&sampler->running, FALSE);
+	return NULL;
+}
+
+#elif defined(__linux__)
 typedef struct CrownInfiniteDragX11Grab
 {
 	Window window;
@@ -332,235 +553,14 @@ static gpointer sample_pointer_x11(gpointer data)
 	return NULL;
 }
 
-#elif defined(_WIN32)
-static const wchar_t WINDOWS_RAW_INPUT_CLASS_NAME[] = L"CrownInfiniteDragRawInput";
-
-/* Event-driven only: do not replace with cursor-position polling or repeated SetCursorPos calls. */
-
-static gboolean register_windows_raw_input(HWND target, gboolean preserve_legacy_events)
-{
-	RAWINPUTDEVICE mouse =
-	{
-		.usUsagePage = 0x01,
-		.usUsage = 0x02,
-		.dwFlags = target != NULL
-			? RIDEV_INPUTSINK | (preserve_legacy_events ? 0 : RIDEV_NOLEGACY)
-			: RIDEV_REMOVE,
-		.hwndTarget = target,
-	};
-	if (RegisterRawInputDevices(&mouse, 1, sizeof(mouse)))
-		return TRUE;
-
-	return FALSE;
-}
-
-static void process_windows_raw_mouse(CrownInfiniteDragSampler *sampler, const RAWMOUSE *mouse)
-{
-	gint delta_x = 0;
-	gint delta_y = 0;
-	if ((mouse->usFlags & MOUSE_MOVE_ABSOLUTE) != 0) {
-		gboolean virtual_desktop = (mouse->usFlags & MOUSE_VIRTUAL_DESKTOP) != 0;
-		gint screen_x = virtual_desktop ? GetSystemMetrics(SM_XVIRTUALSCREEN) : 0;
-		gint screen_y = virtual_desktop ? GetSystemMetrics(SM_YVIRTUALSCREEN) : 0;
-		gint screen_width = virtual_desktop ? GetSystemMetrics(SM_CXVIRTUALSCREEN) : GetSystemMetrics(SM_CXSCREEN);
-		gint screen_height = virtual_desktop ? GetSystemMetrics(SM_CYVIRTUALSCREEN) : GetSystemMetrics(SM_CYSCREEN);
-		gint absolute_x = screen_x + MulDiv(mouse->lLastX, MAX(screen_width - 1, 1), 65535);
-		gint absolute_y = screen_y + MulDiv(mouse->lLastY, MAX(screen_height - 1, 1), 65535);
-		if (sampler->windows_absolute_initialized) {
-			delta_x = absolute_x - sampler->windows_absolute_x;
-			delta_y = absolute_y - sampler->windows_absolute_y;
-		}
-		sampler->windows_absolute_x = absolute_x;
-		sampler->windows_absolute_y = absolute_y;
-		sampler->windows_absolute_initialized = TRUE;
-	} else {
-		delta_x = mouse->lLastX;
-		delta_y = mouse->lLastY;
-		sampler->windows_absolute_initialized = FALSE;
-	}
-
-	if ((mouse->usButtonFlags & RI_MOUSE_WHEEL) != 0) {
-		SHORT wheel_delta = (SHORT)mouse->usButtonData;
-		store_wheel(sampler, 0.0, -(gdouble)wheel_delta/120.0);
-	}
-	if ((mouse->usButtonFlags & RI_MOUSE_HWHEEL) != 0) {
-		SHORT wheel_delta = (SHORT)mouse->usButtonData;
-		store_wheel(sampler, (gdouble)wheel_delta/120.0, 0.0);
-	}
-
-	if (delta_x != 0 || delta_y != 0) {
-		store_delta(sampler, delta_x, delta_y);
-		store_sample(sampler);
-	}
-
-	USHORT cancel_button_down = 0;
-	switch (sampler->cancel_button) {
-	case GDK_BUTTON_PRIMARY:
-		cancel_button_down = RI_MOUSE_LEFT_BUTTON_DOWN;
-		break;
-	case GDK_BUTTON_MIDDLE:
-		cancel_button_down = RI_MOUSE_MIDDLE_BUTTON_DOWN;
-		break;
-	case GDK_BUTTON_SECONDARY:
-		cancel_button_down = RI_MOUSE_RIGHT_BUTTON_DOWN;
-		break;
-	}
-	if (cancel_button_down != 0 && (mouse->usButtonFlags & cancel_button_down) != 0) {
-		g_atomic_int_set(&sampler->cancel_requested, TRUE);
-		g_atomic_int_set(&sampler->running, FALSE);
-		SetEvent(sampler->windows_stop_event);
-		return;
-	}
-
-	USHORT trigger_button_up = 0;
-	switch (sampler->trigger_button) {
-	case GDK_BUTTON_PRIMARY:
-		trigger_button_up = RI_MOUSE_LEFT_BUTTON_UP;
-		break;
-	case GDK_BUTTON_MIDDLE:
-		trigger_button_up = RI_MOUSE_MIDDLE_BUTTON_UP;
-		break;
-	case GDK_BUTTON_SECONDARY:
-		trigger_button_up = RI_MOUSE_RIGHT_BUTTON_UP;
-		break;
-	}
-	if (trigger_button_up != 0 && (mouse->usButtonFlags & trigger_button_up) != 0) {
-		g_atomic_int_set(&sampler->released, TRUE);
-		g_atomic_int_set(&sampler->running, FALSE);
-		SetEvent(sampler->windows_stop_event);
-	}
-}
-
-static LRESULT CALLBACK windows_raw_input_window_proc(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
-{
-	if (message == WM_NCCREATE) {
-		CREATESTRUCTW *create = (CREATESTRUCTW *)lparam;
-		SetWindowLongPtrW(window, GWLP_USERDATA, (LONG_PTR)create->lpCreateParams);
-		return TRUE;
-	}
-
-	CrownInfiniteDragSampler *sampler = (CrownInfiniteDragSampler *)GetWindowLongPtrW(window, GWLP_USERDATA);
-	if (message == WM_INPUT && sampler != NULL) {
-		RAWINPUT input;
-		UINT input_size = sizeof(input);
-		UINT result = GetRawInputData((HRAWINPUT)lparam
-			, RID_INPUT
-			, &input
-			, &input_size
-			, sizeof(RAWINPUTHEADER)
-			);
-		if (result != (UINT)-1 && input.header.dwType == RIM_TYPEMOUSE) {
-			process_windows_raw_mouse(sampler, &input.data.mouse);
-		}
-	}
-
-	return DefWindowProcW(window, message, wparam, lparam);
-}
-
-static HWND create_windows_raw_input_window(CrownInfiniteDragSampler *sampler)
-{
-	HINSTANCE instance = GetModuleHandleW(NULL);
-	WNDCLASSEXW window_class =
-	{
-		.cbSize = sizeof(window_class),
-		.lpfnWndProc = windows_raw_input_window_proc,
-		.hInstance = instance,
-		.lpszClassName = WINDOWS_RAW_INPUT_CLASS_NAME,
-	};
-	if (RegisterClassExW(&window_class) == 0 && GetLastError() != ERROR_CLASS_ALREADY_EXISTS) {
-		return NULL;
-	}
-
-	HWND window = CreateWindowExW(0
-		, WINDOWS_RAW_INPUT_CLASS_NAME
-		, L""
-		, 0
-		, 0
-		, 0
-		, 0
-		, 0
-		, HWND_MESSAGE
-		, NULL
-		, instance
-		, sampler
-		);
-	return window;
-}
-
-static void drain_windows_raw_input_messages(void)
-{
-	MSG message;
-	while (PeekMessageW(&message, NULL, WM_INPUT, WM_INPUT, PM_REMOVE))
-		DispatchMessageW(&message);
-}
-
-static gpointer sample_pointer_windows(gpointer data)
-{
-	CrownInfiniteDragSampler *sampler = data;
-	HWND window = create_windows_raw_input_window(sampler);
-	gboolean registered = FALSE;
-	if (window == NULL)
-		goto setup_failed;
-
-	if (!register_windows_raw_input(window, sampler->preserve_legacy_events))
-		goto setup_failed;
-	registered = TRUE;
-
-	RECT clip =
-	{
-		.left = sampler->anchor_x,
-		.top = sampler->anchor_y,
-		.right = sampler->anchor_x + 1,
-		.bottom = sampler->anchor_y + 1,
-	};
-	if (!ClipCursor(&clip)) {
-		goto setup_failed;
-	}
-
-	while (g_atomic_int_get(&sampler->running)) {
-		DWORD wait_result = MsgWaitForMultipleObjects(1
-			, &sampler->windows_stop_event
-			, FALSE
-			, INFINITE
-			, QS_RAWINPUT
-			);
-		if (wait_result == WAIT_OBJECT_0)
-			break;
-		if (wait_result == WAIT_FAILED) {
-			break;
-		}
-
-		drain_windows_raw_input_messages();
-	}
-
-	/* GTK may observe the release just before this worker does. */
-	drain_windows_raw_input_messages();
-
-	/* Restore and unconfine before the GTK thread reveals the cursor after joining. */
-	SetCursorPos(sampler->anchor_x, sampler->anchor_y);
-	ClipCursor(NULL);
-
-	register_windows_raw_input(NULL, FALSE);
-	DestroyWindow(window);
-	return NULL;
-
-setup_failed:
-	if (registered)
-		register_windows_raw_input(NULL, FALSE);
-	if (window != NULL)
-		DestroyWindow(window);
-	g_atomic_int_set(&sampler->running, FALSE);
-	return NULL;
-}
-
-#endif /* if defined(__linux__) */
+#endif /* if defined(_WIN32) */
 
 static gpointer sample_pointer(gpointer data)
 {
-#if defined(__linux__)
-	return sample_pointer_x11(data);
-#elif defined(_WIN32)
+#if defined(_WIN32)
 	return sample_pointer_windows(data);
+#elif defined(__linux__)
+	return sample_pointer_x11(data);
 #endif
 	return NULL;
 }
@@ -576,7 +576,28 @@ void *crown_infinite_drag_sampler_start(GdkDisplay *display, GdkWindow *window, 
 	sampler->trigger_button = trigger_button;
 	sampler->cancel_button = cancel_button;
 	sampler->preserve_legacy_events = preserve_legacy_events;
-#if defined(__linux__)
+#if defined(_WIN32)
+	(void)display;
+	(void)window;
+	(void)device;
+	POINT cursor_position;
+	if (!GetCursorPos(&cursor_position)) {
+		g_mutex_clear(&sampler->mutex);
+		g_free(sampler);
+		return NULL;
+	}
+	sampler->anchor_x = cursor_position.x;
+	sampler->anchor_y = cursor_position.y;
+	sampler->windows_stop_event = CreateEventW(NULL, TRUE, FALSE, NULL);
+	if (sampler->windows_stop_event == NULL) {
+		g_mutex_clear(&sampler->mutex);
+		g_free(sampler);
+		return NULL;
+	}
+	/* ShowCursor's counter is thread-local, so change it on GTK's UI thread. */
+	ShowCursor(FALSE);
+	sampler->windows_cursor_hidden = TRUE;
+#elif defined(__linux__)
 	sampler->x11_wake_fd = eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK);
 	if (sampler->x11_wake_fd < 0) {
 		g_mutex_clear(&sampler->mutex);
@@ -611,32 +632,11 @@ void *crown_infinite_drag_sampler_start(GdkDisplay *display, GdkWindow *window, 
 		gdk_seat_ungrab(seat);
 		gdk_display_sync(display);
 	}
-#elif defined(_WIN32)
-	(void)display;
-	(void)window;
-	(void)device;
-	POINT cursor_position;
-	if (!GetCursorPos(&cursor_position)) {
-		g_mutex_clear(&sampler->mutex);
-		g_free(sampler);
-		return NULL;
-	}
-	sampler->anchor_x = cursor_position.x;
-	sampler->anchor_y = cursor_position.y;
-	sampler->windows_stop_event = CreateEventW(NULL, TRUE, FALSE, NULL);
-	if (sampler->windows_stop_event == NULL) {
-		g_mutex_clear(&sampler->mutex);
-		g_free(sampler);
-		return NULL;
-	}
-	/* ShowCursor's counter is thread-local, so change it on GTK's UI thread. */
-	ShowCursor(FALSE);
-	sampler->windows_cursor_hidden = TRUE;
 #else
 	(void)display;
 	(void)window;
 	(void)device;
-#endif /* if defined(__linux__) */
+#endif /* if defined(_WIN32) */
 	sampler->thread = g_thread_new("infinite-drag", sample_pointer, sampler);
 	return sampler;
 }
@@ -674,14 +674,14 @@ void crown_infinite_drag_sampler_stop(void *data, gdouble *delta_x, gdouble *del
 {
 	CrownInfiniteDragSampler *sampler = data;
 	g_atomic_int_set(&sampler->running, FALSE);
-#if defined(__linux__)
+#if defined(_WIN32)
+	SetEvent(sampler->windows_stop_event);
+#elif defined(__linux__)
 	if (sampler->x11_wake_fd >= 0) {
 		uint64_t wake_value = 1;
 		while (write(sampler->x11_wake_fd, &wake_value, sizeof(wake_value)) < 0 && errno == EINTR) {
 		}
 	}
-#elif defined(_WIN32)
-	SetEvent(sampler->windows_stop_event);
 #endif
 	g_thread_join(sampler->thread);
 #if defined(_WIN32)
@@ -691,11 +691,11 @@ void crown_infinite_drag_sampler_stop(void *data, gdouble *delta_x, gdouble *del
 	}
 #endif
 	crown_infinite_drag_sampler_drain(sampler, delta_x, delta_y, wheel_dx, wheel_dy, samples);
-#if defined(__linux__)
+#if defined(_WIN32)
+	CloseHandle(sampler->windows_stop_event);
+#elif defined(__linux__)
 	if (sampler->x11_wake_fd >= 0)
 		close(sampler->x11_wake_fd);
-#elif defined(_WIN32)
-	CloseHandle(sampler->windows_stop_event);
 #endif
 	g_mutex_clear(&sampler->mutex);
 	g_free(sampler);
