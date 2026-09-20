7046aede025f78b356e8d53b76ed03be3345bcb2
Daniele Bartolini <dbartolini@crownengine.org>
2026-09-20 17:43:42 +0200

    3rdparty: bullet3: drop unwanted message
    Part-of: #259

--- 3rdparty/bullet3/src/BulletCollision/CollisionDispatch/btCollisionWorld.cpp
+++ 3rdparty/bullet3/src/BulletCollision/CollisionDispatch/btCollisionWorld.cpp
@@ -187,9 +187,6 @@ void btCollisionWorld::updateSingleAabb(btCollisionObject* colObj)
 		{
 			reportMe = false;
 			m_debugDrawer->reportErrorWarning("Overflow in AABB, object removed from simulation");
-			m_debugDrawer->reportErrorWarning("If you can reproduce this, please email bugs@continuousphysics.com\n");
-			m_debugDrawer->reportErrorWarning("Please include above information, your Platform, version of OS.\n");
-			m_debugDrawer->reportErrorWarning("Thanks.\n");
 		}
 	}
 }
